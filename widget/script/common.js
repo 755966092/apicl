533c2da804933783af0fd5bc1b46043d3a609d67
mypc <755966092@qq.com>
2017-04-14 10:56:50 +0800

    test

---
+++ widget/script/common.js
@@ -0,0 +1,66 @@
+/**
+ * Mr.Yuan was create in 2017.4 .13
+ */
+
+//rem转换
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
+
+
+//关闭app
+function exitApp() {
+    api.addEventListener({
+        name: 'keyback'
+    }, function(ret, err) {
+        api.toast({
+            msg: '再按一次退出程序',
+            duration: 2000,
+            location: 'bottom'
+        });
+        api.addEventListener({
+            name: 'keyback'
+        }, function(ret, err) {
+            api.closeWidget({
+                id: 'A6911018044825', //这里改成自己的应用ID
+                retData: {
+                    name: 'closeWidget'
+                },
+                silent: true
+            });
+        });
+        setTimeout(function() {
+            exitApp();
+        }, 2000)
+    });
+}
+
+//初始化
+var header, headerH;
+
+function fnInit() {
+    header = $api.dom('header');
+    $api.fixStatusBar(header);
+    headerH = $api.offset(header).h;
+}
+
+//接口地址
+var apiSite = 'http://kyl.api.app.kuakao.com';
+var apiHeader = {
+    'app-id': 2,
+    'version-id': '2',
+    'did': '19900302',
+    'version-mini': '1',
+    'encrypt-did': '0a78e5dc37c7cb02528747b0e919e4b9'
+};
