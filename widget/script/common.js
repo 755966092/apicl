b3f3d67933db6633b1a92c7cb3155b4ca919435a
mypc <755966092@qq.com>
2017-04-24 14:17:03 +0800

    历史消息列表

--- widget/script/common.js
+++ widget/script/common.js
@@ -66,8 +66,11 @@ var apiHeader = {
 };
 
 // 用户token，融云token
-var userTokenAll = 5,
-    rongTokenAll;
+
+
+var userTokenAll = $api.getStorage('userToken');
+var rongTokenAll = $api.getStorage('rongToken');
+
 
 var rong,
     UIChatTools;
@@ -80,7 +83,7 @@ function moduleInit() {
 }
 
 // 接入融云
-function rongyun(rongToken) {
+function rongyun() {
 
     rong.init(function(ret, err) {
         if (ret.status == 'error')
@@ -93,16 +96,44 @@ function rongyun(rongToken) {
         // 用户1
         // token: 'aAoW4oalHGpvB6Hw89bG0XzZSHvx/Xm6zmi6cWDa3L4VyfNcz/KXDFQxtpoQ+os1nT0799sMXlXPvUAK3FnjIY94cnJzE+aT'
         // 用户2
-        token: 'hjjQ018gh2aPKpdyjqhX0nzZSHvx/Xm6zmi6cWDa3L4VyfNcz/KXDDNYtoRSbb1+nT0799sMXlXSm1rb7lqSfY94cnJzE+aT'
-            // token: rongToken
+        // token: 'hjjQ018gh2aPKpdyjqhX0nzZSHvx/Xm6zmi6cWDa3L4VyfNcz/KXDDNYtoRSbb1+nT0799sMXlXSm1rb7lqSfY94cnJzE+aT'
+        token: rongTokenAll
     }, function(ret, err) {
         // alert(ret.status)
         if (ret.status == 'success')
-            api.toast({
-                msg: ret.result.userId
-            });
+        // api.toast({
+        //     msg:
+
+        // });
+            alert('登录戎云' + ret.result.userId)
+
     });
 
+
+    rong.getConversationList(function(ret, err) {
+        // alert(JSON.stringify(ret.result));err
+        // data = ret.result;
+        // alert('查看历史消息' + JSON.stringify(data))
+        alert('查看历史消息' + JSON.stringify(ret))
+        alert('查看历史消息错误' + JSON.stringify(err))
+            // for (var i = 0; i < ret.result.length; i++) {
+            //     // ret.result[i]
+            //     // 吧头像添加到数据对象中
+            //     data[i].url = '../image/tx_5.jpg';
+
+        //     // 比较最后一条消息的时间戳， 判断是发送的还是接受的
+        //     //-------------发送消息------------------接收消息
+        //     // if (ret.result.receivedTime > ret.result.sentTime) {
+        //         // 发送消息时间戳大， 最后一条是发送的
+        //         // 转换为时分秒
+        //         // 如果是今天只显示时间
+        //         // 如果是昨天显示昨天，不显示时间
+        //         // 否则显示日期
+        //         // 
+        //     // }
+        // }
+        // getData(data);
+    })
 }
 
 function receiveMsg() {
@@ -114,4 +145,27 @@ function receiveMsg() {
             }
         });
     })
+}
+
+
+
+// 计算时分秒
+function changeTime(time) {
+    // 获取的是毫秒数
+    var time = new Date(time);
+    // 秒数
+    var sencond = time.getSeconds()
+        // 分钟数
+    var minute = time.getMinutes()
+        // 小时数
+    var hour = time.getHours()
+        // 年
+    var year = time.getFullYear()
+        // 月
+    var month = time.getMonth()
+        // 日
+    var day = time.getDay()
+        // 星期
+    var xingqu = time.getDate()
+    alert('秒:' + sencond + '分：' + minute + ', 小时' + hour + '，时间' + time)
 }
