e6083421659309bf41147b9fbea5b211a858e4e2
mypc <755966092@qq.com>
2017-04-24 15:36:18 +0800

    历史消息列表2

--- widget/script/common.js
+++ widget/script/common.js
@@ -39,11 +39,21 @@ function exitApp() {
                 },
                 silent: true
             });
+            rong.disconnect({
+                isReceivePush: true
+            }, function(ret, err) {
+                if ('success' == ret.status) {
+                    api.toast({
+                        msg: '断开连接成功!'
+                    });
+                }
+            });
         });
         setTimeout(function() {
             exitApp();
         }, 2000)
     });
+
 }
 
 //初始化
@@ -83,7 +93,7 @@ function moduleInit() {
 }
 
 // 接入融云
-function rongyun() {
+function rongyun(rongToken) {
 
     rong.init(function(ret, err) {
         if (ret.status == 'error')
@@ -95,45 +105,16 @@ function rongyun() {
     rong.connect({
         // 用户1
         // token: 'aAoW4oalHGpvB6Hw89bG0XzZSHvx/Xm6zmi6cWDa3L4VyfNcz/KXDFQxtpoQ+os1nT0799sMXlXPvUAK3FnjIY94cnJzE+aT'
-        // 用户2
+        // 用户2r
         // token: 'hjjQ018gh2aPKpdyjqhX0nzZSHvx/Xm6zmi6cWDa3L4VyfNcz/KXDDNYtoRSbb1+nT0799sMXlXSm1rb7lqSfY94cnJzE+aT'
-        token: rongTokenAll
+        token: rongToken
     }, function(ret, err) {
-        // alert(ret.status)
-        if (ret.status == 'success')
-        // api.toast({
-        //     msg:
+        if (ret.status == 'success') {
+            // alert('登录戎云' + ret.result.userId)
+            // alert('登录' + rongToken)
 
-        // });
-            alert('登录戎云' + ret.result.userId)
-
+        }
     });
-
-
-    rong.getConversationList(function(ret, err) {
-        // alert(JSON.stringify(ret.result));err
-        // data = ret.result;
-        // alert('查看历史消息' + JSON.stringify(data))
-        alert('查看历史消息' + JSON.stringify(ret))
-        alert('查看历史消息错误' + JSON.stringify(err))
-            // for (var i = 0; i < ret.result.length; i++) {
-            //     // ret.result[i]
-            //     // 吧头像添加到数据对象中
-            //     data[i].url = '../image/tx_5.jpg';
-
-        //     // 比较最后一条消息的时间戳， 判断是发送的还是接受的
-        //     //-------------发送消息------------------接收消息
-        //     // if (ret.result.receivedTime > ret.result.sentTime) {
-        //         // 发送消息时间戳大， 最后一条是发送的
-        //         // 转换为时分秒
-        //         // 如果是今天只显示时间
-        //         // 如果是昨天显示昨天，不显示时间
-        //         // 否则显示日期
-        //         // 
-        //     // }
-        // }
-        // getData(data);
-    })
 }
 
 function receiveMsg() {
