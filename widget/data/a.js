8e38a3dc8c7cad7156cd34d520dc3bc224f089f9
mypc <755966092@qq.com>
2017-06-21 18:04:08 +0800

    供应订单流程0621

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,14 +1,67 @@
-06 - 17 16: 06: 49.907 I / app3c(2): {
-	"status": true,
-	"results": ["菏泽牡丹花卉苗木大市场"
-		"菏泽牡丹区曹州玫瑰园"
-		"曹州牡丹园"
-		"菏泽牡丹大酒店"
-		"菏泽市牡丹区教育局"
-		"牡丹区政府"
-		"中国石化(菏泽牡丹区1站)"
-		"菏泽牡丹温泉水世界客房"
-		"中国工商银行(菏泽牡丹支行)"
-		"菏泽牡丹大药房(中意药店)"
-	]
+{
+	"code": 200,
+	"data": {
+		"last_comment": null,
+		"info": {
+			"current_position": null,
+			"status": 1,
+			"title": "测试17",
+			"category_name": "测试分类1",
+			"create_time": "2017-06-20 14:49:41",
+			"user_id": 1,
+			"image_json": "[]",
+			"avg_point": 0,
+			"nickname": "李彤亮",
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+			"city": "海淀区",
+			"sold_count": 0,
+			"type": 1,
+			"current_company": null,
+			"supply_id": 31,
+			"sub_type": 1,
+			"unit": 1,
+			"price": "123.00",
+			"category_id": 1,
+			"form": 1,
+			"comment_count": 0,
+			"description": "测试17"
+		},
+		"is_collect": false
+	},
+	"msg": "",
+	"time": 1498024827
+}
+
+{
+	"code": 200,
+	"data": {
+		"last_comment": null,
+		"info": {
+			"current_position": null,
+			"status": 2,
+			"title": "发布",
+			"category_name": "测试分类1",
+			"create_time": "2017-06-20 09:22:02",
+			"user_id": 1,
+			"image_json": "[]",
+			"avg_point": 0,
+			"nickname": "李彤亮",
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+			"city": "海淀区",
+			"sold_count": 0,
+			"type": 1,
+			"current_company": null,
+			"supply_id": 17,
+			"sub_type": 2,
+			"unit": 4,
+			"price": "50.00",
+			"category_id": 1,
+			"form": 2,
+			"comment_count": 0,
+			"description": "发布"
+		},
+		"is_collect": false
+	},
+	"msg": "",
+	"time": 1498024850
 }
