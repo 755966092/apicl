4d708b2d43d5646943864ce6c02724f18215a4c9
mypc <755966092@qq.com>
2017-06-26 17:26:34 +0800

    求订单

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,57 +1,34 @@
-06 - 26 13: 13: 14.360 I / app3c(2): 上传通讯录数据 {
-	"status": true,
-	"pages": 1,
-	"contacts": [{
-		"firstName": "博泓",
-		"id": 13,
-		"lastName": "孙",
-		"fullName": "孙博泓",
-		"phones": [{
-			"住宅": "159-3269-1211"
-		}]
-	}, {
-		"lastName": "姗鸿",
-		"id": 5,
-		"fullName": "姗鸿",
-		"phones": [{
-			"住宅": "15210771883"
-		}]
-	}, {
-		"firstName": "Junzhuo",
-		"id": 14,
-		"lastName": "Zhao",
-		"email": "app@zgzzzs.com",
-		"fullName": "JunzhuoZhao"
-	}, {
-		"lastName": "刘欣",
-		"id": 1,
-		"fullName": "刘欣",
-		"phones": [{
-			"住宅": "13693238943"
-		}]
-	}, {
-		"lastName": "杨慧",
-		"id": 2,
-		"fullName": "杨慧",
-		"phones": [{
-			"住宅": "18611901229"
-		}]
-	}, {
-		"firstName": "新新",
-		"id": 3,
-		"lastName": "李",
-		"fullName": "李新新",
-		"phones": [{
-			"住宅": "18606615070"
-		}]
-	}, {
-		"firstName": "北京新",
-		"id": 12,
-		"lastName": "鲍思学",
-		"fullName": "鲍思学北京新",
-		"phones": [{
-			"iPhone": "13001232190"
-		}]
-	}],
-	"total": 7
+06 - 26 17: 08: 35.778 I / app3c(2): {
+	"code": 200,
+	"data": {
+		"is_self_add": true,
+		"info": {
+			"current_position": null,
+			"reward": "15.00",
+			"status": "求助中",
+			"title": "111的求",
+			"province": "北京市",
+			"category_name": "测试分类1",
+			"create_time": "2017-06-26 10:59:15",
+			"user_id": 18536,
+			"image_json": [],
+			"demand_id": 15,
+			"end_date": "2019-06-26",
+			"nickname": "111",
+			"head_img_url": "/var/mobile/Containers/Data/Application/EC8A59FD-8743-4118-A649-436D06729280/Library/Caches/APICloud/Cache/getPicture/179501F1-AC64-4E3D-B5E5-33E70085B6E7-19394-00001E7FEFC8CEE5.jpg",
+			"latitude": "40.0008012",
+			"city": "海淀区",
+			"type": "个人",
+			"current_company": null,
+			"longitude": "116.3361315",
+			"category_id": 1,
+			"form": 1,
+			"address": "北京市海淀区中关村东路1号院-3",
+			"description": "测试",
+			"unit": "未定义"
+		},
+		"is_collect": false
+	},
+	"msg": "",
+	"time": 1498468115
 }
