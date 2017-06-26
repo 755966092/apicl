550d4f10227401afb9049931fc9ec1fd4328c246
mypc <755966092@qq.com>
2017-06-26 10:20:19 +0800

    加入评价列表

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,104 +1,40 @@
 {
+	"code": 200,
 	"data": {
-		"reason_list": [],
-		"button_info": {
-			"confirm_finish": {
-				"url": "/api/supply_order/buyerconfirmfinishservice",
-				"is_show": false
-			},
-			"confirm_cancel": {
-				"url": "/api/supply_order/sellerconfirmcancelservice",
-				"is_show": false
-			},
-			"extra": {
-				"url": "/api/supply_order/buyerpayextramoney",
-				"is_show": false
-			},
-			"extend": {
-				"url": "/api/supply_order/buyerextendservice",
-				"is_show": false
-			},
-			"again": {
-				"url": "/api/supply_order/buyercreateorder",
-				"is_show": true
-			},
-			"refuse_cancel": {
-				"url": "/api/supply_order/sellerrefusecancelservice",
-				"is_show": false
-			},
-			"cancel": {
-				"url": "",
-				"is_show": false
-			},
-			"discount": {
-				"url": "/api/supply_order/sellerdiscountservice",
-				"is_show": false
-			},
-			"finish": {
-				"url": "",
-				"is_show": false
-			},
-			"confirm_service": {
-				"url": "/api/supply_order/sellerconfirmservice",
-				"is_show": false
-			},
-			"refund": {
-				"url": "/api/supply_order/sellerrefundservice",
-				"is_show": false
-			},
-			"delete": {
-				"url": "/api/supply_order/buyerdeleteorder",
-				"is_show": true
-			},
-			"pay": {
-				"url": "/api/supply_order/buyerpay",
-				"is_show": false
-			},
-			"comment": {
-				"url": "/api/supply_order/buyercommentservice",
-				"is_show": true
-			},
-			"refuse_service": {
-				"url": "/api/supply_order/sellerrefuseservice",
-				"is_show": false
-			}
-		},
-		"seller_info": {
-			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"list": [{
+			"comment_id": 5,
+			"content": "测试评价",
+			"point": 5,
 			"nickname": "李彤亮",
-			"user_id": 1,
-			"rongcloud_user_id": "c83a957779f8e15eaac8502a198252eb"
-		},
-		"order_info": {
-			"service_time": "2017-07-14 00:00:00",
-			"amount": 1,
-			"reason": "",
-			"discount_money": "0.00",
-			"create_time": "2017-06-22 17:35:05",
-			"money": "1.00",
-			"gen_order_id": "201706221735057960865513",
-			"extra_money": "0.00",
-			"refund_money": "0.00",
-			"order_id": 76,
-			"total_money": 1
-		},
-		"status_info": {
-			"icon": "",
-			"description": "钱款已支付给对方，如有异议请与对方联系",
-			"text": "交易成功"
-		},
-	"identity": "buyer",
-		"supply_info": {
-			"unit": 1,
-			"image": [],
-			"title": "李的供应",
-			"price": "1.00",
-			"supply_id": 38,
-			"form": 1,
-			"description": "购买"
-		}
+			"create_time": "2017-06-23 14:25:02",
+			"is_anon": -1,
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		}, {
+			"comment_id": 8,
+			"content": "评价2",
+			"point": 4,
+			"nickname": "匿名",
+			"create_time": "2017-06-23 14:58:36",
+			"is_anon": 1,
+			"head_img_url": "http://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		}, {
+			"comment_id": 9,
+			"content": "评价",
+			"point": 3,
+			"nickname": "李彤亮",
+			"create_time": "2017-06-23 15:00:26",
+			"is_anon": -1,
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		}, {
+			"comment_id": 10,
+			"content": "评价",
+			"point": 2,
+			"nickname": "匿名",
+			"create_time": "2017-06-23 15:14:52",
+			"is_anon": 1,
+			"head_img_url": "http://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		}]
 	},
 	"msg": "",
-	"time": 1498199794,
-	"code": 200
+	"time": 1498441496
 }
