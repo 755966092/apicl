bae10826c47ab1b61187930546def99b2d8d10ab
mypc <755966092@qq.com>
2017-06-26 18:42:59 +0800

    求助者付款操作

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,34 +1,84 @@
-06 - 26 17: 08: 35.778 I / app3c(2): {
+06 - 26 18: 14: 11.311 I / app3c(2): {
 	"code": 200,
 	"data": {
-		"is_self_add": true,
-		"info": {
-			"current_position": null,
-			"reward": "15.00",
-			"status": "求助中",
-			"title": "111的求",
-			"province": "北京市",
-			"category_name": "测试分类1",
-			"create_time": "2017-06-26 10:59:15",
-			"user_id": 18536,
-			"image_json": [],
-			"demand_id": 15,
-			"end_date": "2019-06-26",
-			"nickname": "111",
-			"head_img_url": "/var/mobile/Containers/Data/Application/EC8A59FD-8743-4118-A649-436D06729280/Library/Caches/APICloud/Cache/getPicture/179501F1-AC64-4E3D-B5E5-33E70085B6E7-19394-00001E7FEFC8CEE5.jpg",
-			"latitude": "40.0008012",
-			"city": "海淀区",
-			"type": "个人",
-			"current_company": null,
-			"longitude": "116.3361315",
-			"category_id": 1,
-			"form": 1,
-			"address": "北京市海淀区中关村东路1号院-3",
-			"description": "测试",
-			"unit": "未定义"
+		"button_info": {
+			"refuse_help": {
+				"is_show": true,
+				"url": "/api/demand_order/hostrefusehelp",
+				"key": "refuse_help"
+			},
+			"finish": {
+				"is_show": false,
+				"url": ""
+			},
+			"confirm_finish": {
+				"is_show": false,
+				"url": "/api/demand_order/hostconfirmfinishhelp"
+			},
+			"cancel_order": {
+				"is_show": false,
+				"url": "/api/demand_order/helpercancelorder"
+			},
+			"cancel": {
+				"is_show": false,
+				"url": ""
+			},
+			"confirm_help": {
+				"is_show": true,
+				"url": "/api/demand_order/hostconfirmhelp",
+				"key": "confirm_help"
+			},
+			"delete": {
+				"is_show": false,
+				"url": ""
+			},
+			"comment": {
+				"is_show": false,
+				"url": ""
+			},
+			"buttonArr": [{
+				"is_show": true,
+				"url": "/api/demand_order/hostrefusehelp",
+				"key": "refuse_help"
+			}, {
+				"is_show": true,
+				"url": "/api/demand_order/hostconfirmhelp",
+				"key": "confirm_help"
+			}]
 		},
-		"is_collect": false
+		"status_info": {
+			"description": "请在5时23分内处理，否则将会关闭订单",
+			"icon": "",
+			"text": "待应答"
+		},
+		"order_info": {
+			"gen_order_id": "201706261737434254322425",
+			"amount": 1,
+			"order_id": 12,
+			"money": "1.00",
+			"create_time": "2017-06-26 17:37:43",
+			"reason": ""
+		},
+		"identity": "host",
+		"helper_info": {
+			"rongcloud_user_id": "8c1971d12d868723cfd1440bfd7bb9dc",
+			"user_id": 18665,
+			"nickname": "222222",
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		},
+		"demand_info": {
+			"reward": "1.00",
+			"form": 1,
+			"title": "测试求助",
+			"demand_id": 17,
+			"image": [],
+			"description": "测试"
+		},
+		"reason_list": {
+			"1": "需求已经完成了",
+			"2": "不想求助了"
+		}
 	},
 	"msg": "",
-	"time": 1498468115
+	"time": 1498472051
 }
