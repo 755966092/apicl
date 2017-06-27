83cf00fa4988406c20519329a34f8c8fbf6d86c2
mypc <755966092@qq.com>
2017-06-27 18:03:27 +0800

    完善求助流程

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,84 +1,48 @@
-06 - 26 18: 14: 11.311 I / app3c(2): {
+{
 	"code": 200,
-	"data": {
-		"button_info": {
-			"refuse_help": {
-				"is_show": true,
-				"url": "/api/demand_order/hostrefusehelp",
-				"key": "refuse_help"
-			},
-			"finish": {
-				"is_show": false,
-				"url": ""
-			},
-			"confirm_finish": {
-				"is_show": false,
-				"url": "/api/demand_order/hostconfirmfinishhelp"
-			},
-			"cancel_order": {
-				"is_show": false,
-				"url": "/api/demand_order/helpercancelorder"
-			},
-			"cancel": {
-				"is_show": false,
-				"url": ""
-			},
-			"confirm_help": {
-				"is_show": true,
-				"url": "/api/demand_order/hostconfirmhelp",
-				"key": "confirm_help"
-			},
-			"delete": {
-				"is_show": false,
-				"url": ""
-			},
-			"comment": {
-				"is_show": false,
-				"url": ""
-			},
-			"buttonArr": [{
-				"is_show": true,
-				"url": "/api/demand_order/hostrefusehelp",
-				"key": "refuse_help"
-			}, {
-				"is_show": true,
-				"url": "/api/demand_order/hostconfirmhelp",
-				"key": "confirm_help"
-			}]
-		},
-		"status_info": {
-			"description": "请在5时23分内处理，否则将会关闭订单",
-			"icon": "",
-			"text": "待应答"
-		},
-		"order_info": {
-			"gen_order_id": "201706261737434254322425",
-			"amount": 1,
-			"order_id": 12,
-			"money": "1.00",
-			"create_time": "2017-06-26 17:37:43",
-			"reason": ""
-		},
-		"identity": "host",
-		"helper_info": {
-			"rongcloud_user_id": "8c1971d12d868723cfd1440bfd7bb9dc",
-			"user_id": 18665,
-			"nickname": "222222",
-			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
-		},
-		"demand_info": {
-			"reward": "1.00",
-			"form": 1,
-			"title": "测试求助",
-			"demand_id": 17,
-			"image": [],
-			"description": "测试"
-		},
-		"reason_list": {
-			"1": "需求已经完成了",
-			"2": "不想求助了"
-		}
-	},
 	"msg": "",
-	"time": 1498472051
+	"time": 1498547113,
+	"data": {
+		"list": [{
+			"demand_id": 22,
+			"title": "1",
+			"type": 1,
+			"reward": "1.00",
+			"description": "啊",
+			"image_json": "[]",
+			"status": 3
+		}, {
+			"demand_id": 21,
+			"title": "11",
+			"type": 1,
+			"reward": "1.00",
+			"description": "22",
+			"image_json": "[]",
+			"status": 1
+		}, {
+			"demand_id": 20,
+			"title": "测试5",
+			"type": 1,
+			"reward": "1.00",
+			"description": "测试",
+			"image_json": "[]",
+			"status": 1
+		}, {
+			"demand_id": 19,
+			"title": "测试求额的3",
+			"type": 1,
+			"reward": "1.00",
+			"description": "BS",
+			"image_json": "[]",
+			"status": 3
+		}, {
+			"demand_id": 18,
+			"title": "111的求2",
+			"type": 1,
+			"reward": "1.00",
+			"description": "测试",
+			"image_json": "[]",
+			"status": 3
+		}]
+	}
 }
