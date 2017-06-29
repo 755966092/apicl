65f6de78dedb753e07e44b9bbe9c60a38796a5ae
mypc <755966092@qq.com>
2017-06-29 18:10:19 +0800

    订单跳到聊天页面06-29

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,27 +1,104 @@
- A: [{
- 		title: '北京大学'
- 	}, {
- 		title: '北京理工大学'
- 	}, {
- 		title: '北京师范大学'
- 	}],
- 	Q: [{
- 		title: '清华大学'
- 	}, {
- 		title: '青岛海洋大学'
- 	}],
- 	N: [{
- 		title: '南开大学'
- 	}],
- 	F: [{
- 		title: '复旦大学'
- 	}],
- 	S: [{
- 		title: '山东科技大学'
- 	}],
- 	Z: [{
- 		title: '中石油大学'
- 	}],
- 	T: [{
- 		title: '同济大学'
- 	}]
+06 - 29 17: 44: 35.450 I / app3c(2): 获取的数据: {
+	"code": 200,
+	"data": {
+		"button_info": {
+			"extra": {
+				"is_show": false,
+				"url": "/api/supply_order/buyerpayextramoney"
+			},
+			"discount": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerdiscountservice"
+			},
+			"finish": {
+				"is_show": false,
+				"url": ""
+			},
+			"confirm_service": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerconfirmservice"
+			},
+			"cancel": {
+				"is_show": false,
+				"url": ""
+			},
+			"confirm_finish": {
+				"is_show": false,
+				"url": "/api/supply_order/buyerconfirmfinishservice"
+			},
+			"refuse_cancel": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerrefusecancelservice"
+			},
+			"comment": {
+				"is_show": true,
+				"url": "/api/supply_order/buyercommentservice"
+			},
+			"delete": {
+				"is_show": true,
+				"url": "/api/supply_order/buyerdeleteorder"
+			},
+			"pay": {
+				"is_show": false,
+				"url": "/api/supply_order/buyerpay"
+			},
+			"again": {
+				"is_show": false,
+				"url": "/api/supply_order/buyercreateorder"
+			},
+			"refuse_service": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerrefuseservice"
+			},
+			"extend": {
+				"is_show": false,
+				"url": "/api/supply_order/buyerextendservice"
+			},
+			"refund": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerrefundservice"
+			},
+			"confirm_cancel": {
+				"is_show": false,
+				"url": "/api/supply_order/sellerconfirmcancelservice"
+			}
+		},
+		"status_info": {
+			"description": "订单已取消，钱款将自动退回",
+			"icon": "",
+			"text": "订单关闭"
+		},
+		"order_info": {
+			"amount": 1,
+			"order_id": 92,
+			"gen_order_id": "201706291545534370798408",
+			"extra_money": "0.00",
+			"total_money": 1,
+			"reason": null,
+			"discount_money": "0.00",
+			"refund_money": "0.00",
+			"create_time": "2017-06-29 15:45:53",
+			"service_time": "2017-06-29 15:45:00",
+			"money": "1.00"
+		},
+		"identity": "buyer",
+		"reason_list": [],
+		"seller_info": {
+			"rongcloud_user_id": "c83a957779f8e15eaac8502a198252eb",
+			"user_id": 1,
+			"nickname": "李彤亮",
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+		},
+		"supply_info": {
+			"unit": 2,
+			"price": "1.00",
+			"title": "我我发布",
+			"supply_id": 42,
+			"image": [],
+			"description": "测试",
+			"form": 1
+		}
+	},
+	"msg": "",
+	"time": 1498729478
+}
