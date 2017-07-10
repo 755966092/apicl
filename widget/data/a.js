060c36a65e8ae5245be16abfcb3755fd7e5e20bc
mypc <755966092@qq.com>
2017-07-10 18:12:40 +0800

    备注缓存07-10

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,32 +1,39 @@
-07 - 07 16: 20: 48.157 I / app3c(2): {
-	"code": 200,
-	"data": {
-		"status": [],
-		"info": {
-			"current_position": null,
-			"current_degree": null,
-			"nickname": null,
-			"real_name": null,
-			"birthday": null,
-			"user_id": 18665,
-			"hometown": null,
-			"sex": null,
-			"head_img_url": null,
-			"current_company": null
-		},
-		"extra": null,
-		"contact": {
-			"update_time": "2017-07-07 10:29:56",
-			"phone": "13260227521",
-			"sub_type": 1,
-			"is_default": 1,
-			"remark_user_id": 18536,
-			"create_time": "2017-07-07 10:29:56",
-			"user_id": 18665,
-			"type": 1,
-			"contact_id": 3
-		}
-	},
-	"msg": "",
-	"time": 1499415647
+{
+	"data": [{
+		"list_id": "18665_undefined",
+		"user_id": "18665",
+		"other_contact_id": "3",
+		"other_info": "123456",
+		"other_type": "1",
+		"other_sub_type": "2"
+	}, {
+		"list_id": "18665_7",
+		"user_id": "18665",
+		"other_contact_id": "7",
+		"other_info": "wwwwwwww",
+		"other_type": "2",
+		"other_sub_type": "1"
+	}, {
+		"list_id": "18665_6",
+		"user_id": "18665",
+		"other_contact_id": "6",
+		"other_info": "安卓给苹果备注网站",
+		"other_type": "2",
+		"other_sub_type": "1"
+	}, {
+		"list_id": "18665_4",
+		"user_id": "18665",
+		"other_contact_id": "4",
+		"other_info": "安卓给苹果即时通讯",
+		"other_type": "1",
+		"other_sub_type": "2"
+	}, {
+		"list_id": "18665_3",
+		"user_id": "18665",
+		"other_contact_id": "3",
+		"other_info": "123456",
+		"other_type": "1",
+		"other_sub_type": "2"
+	}],
+	"status": true
 }
