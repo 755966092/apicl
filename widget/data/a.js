167cfc7987a46a1c9c2501c9d0ad0af25cc23e6d
mypc <755966092@qq.com>
2017-07-12 11:06:43 +0800

    备注内容显示07-12

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,39 +1,147 @@
 {
-	"data": [{
-		"list_id": "18665_undefined",
-		"user_id": "18665",
-		"other_contact_id": "3",
-		"other_info": "123456",
-		"other_type": "1",
-		"other_sub_type": "2"
-	}, {
-		"list_id": "18665_7",
-		"user_id": "18665",
-		"other_contact_id": "7",
-		"other_info": "wwwwwwww",
-		"other_type": "2",
-		"other_sub_type": "1"
-	}, {
-		"list_id": "18665_6",
-		"user_id": "18665",
-		"other_contact_id": "6",
-		"other_info": "安卓给苹果备注网站",
-		"other_type": "2",
-		"other_sub_type": "1"
-	}, {
-		"list_id": "18665_4",
-		"user_id": "18665",
-		"other_contact_id": "4",
-		"other_info": "安卓给苹果即时通讯",
-		"other_type": "1",
-		"other_sub_type": "2"
-	}, {
-		"list_id": "18665_3",
-		"user_id": "18665",
-		"other_contact_id": "3",
-		"other_info": "123456",
-		"other_type": "1",
-		"other_sub_type": "2"
-	}],
-	"status": true
+	"relationship_level": 3,
+	"show_status": {
+		"rongcloud": true,
+		"contact": true
+	},
+	"info": {
+		"user_id": 18661,
+		"nickname": "北京—丹姐",
+		"real_name": null,
+		"sex": null,
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"current_company": null,
+		"current_position": null,
+		"hometown": null,
+		"birthday": null,
+		"rongcloud_user_id": null
+	},
+	"contact": {
+		"contact_id": 119,
+		"create_time": "2017-07-10 10:07:10",
+		"update_time": "2017-07-10 10:07:10",
+		"user_id": 18661,
+		"phone": "18811523253",
+		"type": 1,
+		"sub_type": 2,
+		"is_default": 1
+	},
+	"extra": {
+		"extra_id": 6,
+		"remark_user_id": 18661,
+		"content": "北京背背",
+		"image_url": "/storage/emulated/0/Android/data/com.iinnet.apicloud/cache/disk/thumb/89f2ab38"
+	},
+	"status": []
+}
+
+
+
+{
+	"code": 200,
+	"msg": "",
+	"time": 1499827838,
+	"data": {
+		"info": {
+			"user_id": 18536,
+			"nickname": "北京—丹姐",
+			"real_name": "备注3",
+			"sex": 2,
+			"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+			"hometown": "天津-天津",
+			"birthday": "2017-07-22",
+			"current_company": null,
+			"current_position": null,
+			"current_degree": null
+		},
+		"extra": {
+			"extra_id": 6,
+			"remark_user_id": 18661,
+			"content": "北京背背",
+			"image_url": "/storage/emulated/0/Android/data/com.iinnet.apicloud/cache/disk/thumb/89f2ab38"
+		},
+		"status": [],
+		"contact_list": [{
+			"contact_id": 45,
+			"phone": "13260227521",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
+		}, {
+			"contact_id": 44,
+			"phone": "123456",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
+		}, {
+			"contact_id": 39,
+			"phone": "13260227524",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
+		}, {
+			"contact_id": 38,
+			"phone": "13260227523",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
+		}, {
+			"contact_id": 37,
+			"phone": "13260227522",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": 1
+		}],
+		"other_contact_list": [{
+			"contact_id": 9,
+			"info": "www.ww.www",
+			"type": 2,
+			"sub_type": 1
+		}, {
+			"contact_id": 8,
+			"info": "123456",
+			"type": 1,
+			"sub_type": 2
+		}],
+		"address_list": [],
+		"work_list": [{
+			"work_id": 10,
+			"company_id": 0,
+			"company_name": "测试",
+			"position": "测试",
+			"description": "测试",
+			"start_date": "2017-06-11",
+			"end_date": "2017-06-23",
+			"is_default": -1
+		}],
+		"education_list": [{
+			"education_id": 9,
+			"type": 3,
+			"school": "清华",
+			"major": "备注",
+			"degree": 3,
+			"description": "备注",
+			"start_date": "2017-06-11",
+			"end_date": "2017-06-30"
+		}, {
+			"education_id": 8,
+			"type": 3,
+			"school": "北大",
+			"major": "备注缓存",
+			"degree": 3,
+			"description": "备注",
+			"start_date": "2017-06-11",
+			"end_date": "2017-06-30"
+		}, {
+			"education_id": 7,
+			"type": 2,
+			"school": "测试",
+			"major": "理科",
+			"degree": 0,
+			"description": "测试",
+			"start_date": "2017-06-11",
+			"end_date": "2017-06-23"
+		}],
+		"projectList": []
+	}
 }
