5831c8d1cf61f5b3faec2cb3c75dcc37b8d224f5
mypc <755966092@qq.com>
2017-07-13 15:54:42 +0800

    集成极光推送07-13

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,8 +1,19 @@
-07 - 13 09: 19: 41.660 I / app3c(2): {
-	"code": 200,
-	"data": {
-		"config": "alipay_sdk=alipay-sdk-php-20161101&amp;app_id=2017070407638618&amp;biz_content=%7B%22body%22%3A%22%E6%B5%8B%E8%AF%95%E5%BE%AE%E4%BF%A1%E6%94%AF%E4%BB%98%22%2C%22subject%22%3A+%22%E6%B5%8B%E8%AF%95%E5%BE%AE%E4%BF%A1%E6%94%AF%E4%BB%98%22%2C%22out_trade_no%22%3A+%221499908784394%22%2C%22timeout_express%22%3A+%2230m%22%2C%22total_amount%22%3A+%220.01%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%7D&amp;charset=UTF-8&amp;format=json&amp;method=alipay.trade.app.pay&amp;notify_url=https%3A%2F%2Fwozai.tonyliangli.cn%2F%2Fapi%2Fpay_handle%2Fsupplyorderalipay&amp;sign_type=RSA2&amp;timestamp=2017-07-13+09%3A19%3A44&amp;version=1.0&amp;sign=l27jwpfC9AOYqm4kTfuKUaZnR1zTrdolgD6LT8f2n1mpQE7kblQ7CSw3xapjDsgHYJH0q7JdsWsMQ%2BXwJpsZqYm80eSu2sZnAJn7kMIS9N1dPgjhrcrN2eDcECVJdvkbcbbzKG%2FkW31h9LMnWa6OfPRS7ViAt77FaT0FajO0dbdSIEZMovr3ykbE7ICB9wI9FGgGdobNzQ0ipT3rHSf1r9e6L7vs8iWoceOs3e9DA1yp%2BSZWR%2FIw4q6Al8lE4Pvo8eqS2fStOEcT1CHR%2F5szAwShD4%2BbEnC5h4SR%2BEGvdnShsnrI1gu2F%2FesNrOdTa49HCofi4RQAllU1NinnTU0%2Bw%3D%3D"
-	},
-	"msg": "",
-	"time": 1499908784
-}
+[{
+	"member_id": 33,
+	"user_id": 18536,
+	"note": "",
+	"nickname": "111",
+	"head_img_url": "http://img1.skqkw.cn:888/2014/12/08/07/rf5rlvjegvz-10335.jpg"
+}, {
+	"member_id": 38,
+	"user_id": 18668,
+	"note": "",
+	"nickname": "33333",
+	"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+}, {
+	"member_id": 40,
+	"user_id": 18669,
+	"note": "",
+	"nickname": "555555",
+	"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png"
+}]
