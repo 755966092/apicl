e65876ad8703e34fb974444a72f15e8bf2421b0d
mypc <755966092@qq.com>
2017-07-15 18:07:01 +0800

    备注资料07-15

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,12 +1,37 @@
-{
-	"code": 200,
-	"msg": "",
-	"time": 1500083522,
-	"data": {
-		"balance": "52.00",
-		"is_set_pay_password": false,
-		"is_set_alipay": false,
-		"alipay_info": []
-	}
+124::: {
+	"data": [{
+		"remark_user_id": "18659",
+		"current_position": "",
+		"current_degree": "",
+		"current_company": "",
+		"nickname": "a杨总aaaa",
+		"real_name": "",
+		"birthday": "",
+		"hometown": "",
+		"sex": "2",
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"extra_id": "",
+		"extra_content": "",
+		"extra_image_url": ""
+	}],
+	"status": true
 }
-at me_hy_txInfo.html: 160
+at txl_addFellow_details_frame.html: 124
+
+通讯录数据库::::: {
+	"data": [{
+		"user_id": "18659",
+		"nickname": "a杨总",
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"current_position": "",
+		"current_company": "",
+		"titleSize": "15",
+		"title": "a杨总",
+		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"flag": "A",
+		"litter": "AYZ",
+		"type": "3"
+	}],
+	"status": true
+}
+at txl_addFellow_details_frame.html: 194
