8a5ba598331226d7955c4273e4458dc9cbb2365c
mypc <755966092@qq.com>
2017-07-15 15:30:51 +0800

    支付密码功能07-15

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,19 +1,12 @@
 {
-	"supply_id": 9,
-	"title": "测试图片5",
-	"type": 1,
-	"sub_type": 2,
-	"price": "1.00",
-	"create_time": "38分钟之前",
-	"unit": "幅",
-	"description": "测试",
-	"company_name": "",
-	"image_json": ["http://ot0c8ontv.bkt.clouddn.com/FnM3IGuuH0C_zOWlYvX5N9F2qiLs"],
-	"latitude": "40.0007859",
-	"longitude": "116.3361078",
-	"nickname": "111",
-	"sex": 2,
-	"head_img_url": "http://ot0c8ontv.bkt.clouddn.com/Fh2PcIEgQ8kCu2pnRUobTo0WYiIi",
-	"distance": null
+	"code": 200,
+	"msg": "",
+	"time": 1500083522,
+	"data": {
+		"balance": "52.00",
+		"is_set_pay_password": false,
+		"is_set_alipay": false,
+		"alipay_info": []
+	}
 }
-at supply_publish_1.html: 444
+at me_hy_txInfo.html: 160
