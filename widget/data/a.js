df24e932c69c13301ea406146023924622d17cce
mypc <755966092@qq.com>
2017-07-17 17:45:05 +0800

    修改内容07-17

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,37 +1,23 @@
-124::: {
-	"data": [{
-		"remark_user_id": "18659",
-		"current_position": "",
-		"current_degree": "",
-		"current_company": "",
-		"nickname": "a杨总aaaa",
+性别2::::: {
+	"info": {
+		"user_id": 296,
+		"nickname": "qwertyrttt",
 		"real_name": "",
-		"birthday": "",
-		"hometown": "",
-		"sex": "2",
+		"sex": 1,
 		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"extra_id": "",
-		"extra_content": "",
-		"extra_image_url": ""
-	}],
-	"status": true
+		"hometown": null,
+		"birthday": null,
+		"current_company": null,
+		"current_position": null,
+		"current_degree": null
+	},
+	"extra": null,
+	"status": [],
+	"contact_list": [],
+	"other_contact_list": [],
+	"address_list": [],
+	"work_list": [],
+	"education_list": [],
+	"projectList": []
 }
-at txl_addFellow_details_frame.html: 124
-
-通讯录数据库::::: {
-	"data": [{
-		"user_id": "18659",
-		"nickname": "a杨总",
-		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"current_position": "",
-		"current_company": "",
-		"titleSize": "15",
-		"title": "a杨总",
-		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"flag": "A",
-		"litter": "AYZ",
-		"type": "3"
-	}],
-	"status": true
-}
-at txl_addFellow_details_frame.html: 194
+at txl_memberInfo_Frame.html: 618
