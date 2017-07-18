61e8d489aadf2d0bd095602574ffe2339ae711ba
mypc <755966092@qq.com>
2017-07-18 20:02:00 +0800

    完善编辑删除备注联系方式07-18

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,16 +1,29 @@
-07 - 18 18: 14: 13.986 I / app3c(2): {
-	"code": 200,
-	"data": {
-		"info": {
-			"phone": "13260227523",
-			"sub_type": 1,
-			"is_default": 1,
-			"remark_user_id": 18670,
-			"user_id": 18665,
-			"type": 1,
-			"contact_id": 53
-		}
-	},
-	"msg": "",
-	"time": 1500372855
-}
+07 - 18 19: 35: 35.074 I / app3c(2): 本地数据库[{
+	"phone": "13260227521",
+	"sub_type": "工作",
+	"is_default": 1,
+	"list_id": "18670_56",
+	"user_id": 18670,
+	"type": 1,
+	"contact_id": 56,
+	"flag": 1
+}, {
+	"phone": "13260227522",
+	"sub_type": "工作",
+	"is_default": -1,
+	"list_id": "18670_57",
+	"user_id": 18670,
+	"type": 2,
+	"contact_id": 57,
+	"flag": 2
+}]
+07 - 18 19: 37: 55.304 I / app3c(2): 本地数据库[{
+	"phone": "13260227522",
+	"sub_type": "工作",
+	"is_default": -1,
+	"list_id": "18670_57",
+	"user_id": 18670,
+	"type": 2,
+	"contact_id": 57,
+	"flag": 2
+}]
