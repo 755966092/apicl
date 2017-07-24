6c45a62b630f8d7f46caecc062823688ccee26ae
mypc <755966092@qq.com>
2017-07-24 16:03:23 +0800

    当前状态选择07-24

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,50 +1,37 @@
-663::::::::::: {
-	"data": [{
-		"user_id": "18670",
-		"nickname": "177777",
-		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"current_position": "",
-		"current_company": "",
-		"current_degree": "",
-		"current_contact": "",
-		"titleSize": "15",
-		"title": "177777",
-		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"flag": "1",
-		"litter": "177777",
-		"type": "1",
-		"sex": "2",
-		"remark_nickname": "",
-		"remark_real_name": "",
-		"remark_head_img_url": "",
-		"remark_sex": "",
-		"remark_birthday": "2017-07-29",
-		"remark_hometown": "北京-东城区",
-		"remark_current_company": "",
-		"remark_current_position": "",
-		"remark_current_degree": "",
-		"remark_current_contact": ""
-	}],
-	"status": true
-}
-at txl_memberInfo_Frame.html: 531
-
-607::: {
-	"data": [{
-		"remark_user_id": "18670",
-		"current_position": "",
-		"current_degree": "",
-		"current_company": "",
-		"nickname": "",
-		"real_name": "",
-		"birthday": "",
-		"hometown": "",
-		"sex": "",
-		"head_img_url": "",
-		"extra_id": "10",
-		"extra_content": "备注",
-		"extra_image_url": "http://ot0c8ontv.bkt.clouddn.com/FlcBdkCI44Zg6yc5TJk9zb7K-N1x"
-	}],
-	"status": true
-}
-at txl_memberInfo_Frame.html: 607
+[{
+	"status_id": 23,
+	"user_id": 18536,
+	"name": "第一学",
+	"position": "开发",
+	"degree": "",
+	"start_date": "2017-04-12",
+	"is_default": 1,
+	"type": 1
+}, {
+	"status_id": 47,
+	"user_id": 18536,
+	"name": "北大",
+	"position": "信息工程",
+	"degree": "硕士",
+	"start_date": "2016-06-07",
+	"is_default": -1,
+	"type": 2
+}] at txl_memberInfo_Frame.html: 559[{
+	"status_id": 23,
+	"user_id": 18536,
+	"name": "第一学",
+	"position": "开发",
+	"degree": "",
+	"start_date": "2017-04-12",
+	"is_default": 1,
+	"type": 1
+}, {
+	"status_id": 47,
+	"user_id": 18536,
+	"name": "北大",
+	"position": "信息工程",
+	"degree": "硕士",
+	"start_date": "2016-06-07",
+	"is_default": -1,
+	"type": 2
+}] at txl_memberInfo_Frame.html: 559
