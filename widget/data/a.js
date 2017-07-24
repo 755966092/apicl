8bcd1032eea6d9ed7088f72fe01d950ed71a88d7
mypc <755966092@qq.com>
2017-07-24 16:29:11 +0800

    显示默认电话07-24

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,37 +1,62 @@
-[{
-	"status_id": 23,
-	"user_id": 18536,
-	"name": "第一学",
-	"position": "开发",
-	"degree": "",
-	"start_date": "2017-04-12",
-	"is_default": 1,
-	"type": 1
-}, {
-	"status_id": 47,
-	"user_id": 18536,
-	"name": "北大",
-	"position": "信息工程",
-	"degree": "硕士",
-	"start_date": "2016-06-07",
-	"is_default": -1,
-	"type": 2
-}] at txl_memberInfo_Frame.html: 559[{
-	"status_id": 23,
-	"user_id": 18536,
-	"name": "第一学",
-	"position": "开发",
-	"degree": "",
-	"start_date": "2017-04-12",
-	"is_default": 1,
-	"type": 1
-}, {
-	"status_id": 47,
-	"user_id": 18536,
-	"name": "北大",
-	"position": "信息工程",
-	"degree": "硕士",
-	"start_date": "2016-06-07",
-	"is_default": -1,
-	"type": 2
-}] at txl_memberInfo_Frame.html: 559
+663::::::::::: {
+	"data": [{
+		"user_id": "18668",
+		"nickname": "33333",
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"current_position": "",
+		"current_company": "",
+		"current_degree": "",
+		"current_contact": "",
+		"titleSize": "15",
+		"title": "33333",
+		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"flag": "3",
+		"litter": "33333",
+		"type": "1",
+		"sex": "1",
+		"remark_nickname": "",
+		"remark_real_name": "",
+		"remark_head_img_url": "",
+		"remark_sex": "",
+		"remark_birthday": "",
+		"remark_hometown": "",
+		"remark_current_company": "",
+		"remark_current_position": "",
+		"remark_current_degree": "",
+		"remark_current_contact": "",
+		"current_status": ""
+	}],
+	"status": true
+}
+at txl_memberInfo_Frame.html: 652
+663::::::::::: {
+	"data": [{
+		"user_id": "18645",
+		"nickname": "室友—炜佳",
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"current_position": "",
+		"current_company": "",
+		"current_degree": "",
+		"current_contact": "",
+		"titleSize": "15",
+		"title": "室友—炜佳",
+		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"flag": "S",
+		"litter": "SY—WJ",
+		"type": "3",
+		"sex": "1",
+		"remark_nickname": "",
+		"remark_real_name": "",
+		"remark_head_img_url": "",
+		"remark_sex": "",
+		"remark_birthday": "",
+		"remark_hometown": "",
+		"remark_current_company": "",
+		"remark_current_position": "",
+		"remark_current_degree": "",
+		"remark_current_contact": "",
+		"current_status": ""
+	}],
+	"status": true
+}
+at txl_memberInfo_Frame.html: 652
