e6bdaf2300fee7d1a1d1888beb7041bbefb683e6
mypc <755966092@qq.com>
2017-07-24 14:07:40 +0800

    备注信息07-24

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,38 +1,50 @@
-{
+663::::::::::: {
 	"data": [{
-		"list_id": "18665_10",
-		"user_id": "18665",
-		"project_id": "10",
-		"name": "测试备注",
-		"url": "测试",
-		"role": "测试",
-		"description": "测试",
-		"achievement": "测试",
-		"start_date": "2017-06-24",
-		"end_date": "2017-06-30"
-	}, {
-		"list_id": "18665_7",
-		"user_id": "18665",
-		"project_id": "7",
-		"name": "备注",
-		"url": "备注",
-		"role": "备注",
-		"description": "备注",
-		"achievement": "备注",
-		"start_date": "2017-06-12",
-		"end_date": "2017-06-30"
-	}, {
-		"list_id": "18665_2",
-		"user_id": "18665",
-		"project_id": "2",
-		"name": "备注",
-		"url": "备注",
-		"role": "备注",
-		"description": "备注",
-		"achievement": "备注",
-		"start_date": "2017-06-07",
-		"end_date": "2017-06-17"
+		"user_id": "18670",
+		"nickname": "177777",
+		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"current_position": "",
+		"current_company": "",
+		"current_degree": "",
+		"current_contact": "",
+		"titleSize": "15",
+		"title": "177777",
+		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
+		"flag": "1",
+		"litter": "177777",
+		"type": "1",
+		"sex": "2",
+		"remark_nickname": "",
+		"remark_real_name": "",
+		"remark_head_img_url": "",
+		"remark_sex": "",
+		"remark_birthday": "2017-07-29",
+		"remark_hometown": "北京-东城区",
+		"remark_current_company": "",
+		"remark_current_position": "",
+		"remark_current_degree": "",
+		"remark_current_contact": ""
 	}],
 	"status": true
 }
-at txl_addFellow_project_frame.html: 195
+at txl_memberInfo_Frame.html: 531
+
+607::: {
+	"data": [{
+		"remark_user_id": "18670",
+		"current_position": "",
+		"current_degree": "",
+		"current_company": "",
+		"nickname": "",
+		"real_name": "",
+		"birthday": "",
+		"hometown": "",
+		"sex": "",
+		"head_img_url": "",
+		"extra_id": "10",
+		"extra_content": "备注",
+		"extra_image_url": "http://ot0c8ontv.bkt.clouddn.com/FlcBdkCI44Zg6yc5TJk9zb7K-N1x"
+	}],
+	"status": true
+}
+at txl_memberInfo_Frame.html: 607
