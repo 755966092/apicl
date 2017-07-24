cda91e9240125fe3bcaab6b603552036096db7ec
mypc <755966092@qq.com>
2017-07-24 19:53:27 +0800

    供求搜索筛选07-24

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,62 +1,115 @@
-663::::::::::: {
-	"data": [{
-		"user_id": "18668",
-		"nickname": "33333",
-		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"current_position": "",
-		"current_company": "",
-		"current_degree": "",
-		"current_contact": "",
-		"titleSize": "15",
-		"title": "33333",
-		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"flag": "3",
-		"litter": "33333",
-		"type": "1",
-		"sex": "1",
-		"remark_nickname": "",
-		"remark_real_name": "",
-		"remark_head_img_url": "",
-		"remark_sex": "",
-		"remark_birthday": "",
-		"remark_hometown": "",
-		"remark_current_company": "",
-		"remark_current_position": "",
-		"remark_current_degree": "",
-		"remark_current_contact": "",
-		"current_status": ""
-	}],
-	"status": true
+{
+	"code": 200,
+	"msg": "",
+	"time": 1500894670,
+	"data": {
+		"relationship_level": 4,
+		"show_status": {
+			"rongcloud": true,
+			"contact": true
+		},
+		"info": {
+			"user_id": 18536,
+			"nickname": "111",
+			"real_name": "我们的团队",
+			"sex": 2,
+			"head_img_url": "http://ot0c8ontv.bkt.clouddn.com/Fmg6Owc-tHR4CjxOvQDdM6JVuki6",
+			"current_company": "第一学",
+			"current_position": "开发",
+			"hometown": "广西-南宁",
+			"birthday": "2023-06-30",
+			"status": 1,
+			"rongcloud_user_id": "5b81284b29a52e022442207441f0025b"
+		},
+		"contact": {
+			"contact_id": 78,
+			"phone": "13260227521"
+		},
+		"extra": {
+			"extra_id": 11,
+			"remark_user_id": 18536,
+			"content": "备注",
+			"image_url": "http://ot0c8ontv.bkt.clouddn.com/Fr95UrPxGy5cH_mvk6mOlaHSC3iV"
+		},
+		"status": [{
+			"status_id": 23,
+			"user_id": 18536,
+			"name": "第一学",
+			"position": "开发",
+			"degree": "",
+			"start_date": "2017-04-12",
+			"is_default": 1,
+			"type": 1
+		}, {
+			"status_id": 47,
+			"user_id": 18536,
+			"name": "北大",
+			"position": "信息工程",
+			"degree": "3",
+			"start_date": "2016-06-07",
+			"is_default": -1,
+			"type": 2
+		}]
+	}
 }
-at txl_memberInfo_Frame.html: 652
-663::::::::::: {
-	"data": [{
-		"user_id": "18645",
-		"nickname": "室友—炜佳",
-		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"current_position": "",
-		"current_company": "",
-		"current_degree": "",
-		"current_contact": "",
-		"titleSize": "15",
-		"title": "室友—炜佳",
-		"img": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"flag": "S",
-		"litter": "SY—WJ",
-		"type": "3",
-		"sex": "1",
-		"remark_nickname": "",
-		"remark_real_name": "",
-		"remark_head_img_url": "",
-		"remark_sex": "",
-		"remark_birthday": "",
-		"remark_hometown": "",
-		"remark_current_company": "",
-		"remark_current_position": "",
-		"remark_current_degree": "",
-		"remark_current_contact": "",
-		"current_status": ""
-	}],
-	"status": true
+at txl_memberInfo_Frame.html: 561
+
+{
+	"code": 200,
+	"msg": "",
+	"time": 1500896920,
+	"data": {
+		"info": {
+			"user_id": 18665,
+			"nickname": "",
+			"real_name": "",
+			"sex": "",
+			"head_img_url": "",
+			"hometown": "",
+			"birthday": "",
+			"current_company": "",
+			"current_position": "",
+			"current_degree": ""
+		},
+		"extra": null,
+		"status": [{
+			"status_id": 33,
+			"user_id": 18665,
+			"remark_user_id": 18669,
+			"name": "当前",
+			"position": "理科",
+			"degree": "0",
+			"start_date": "2017-06-24",
+			"is_default": -1,
+			"type": 2
+		}],
+		"contact_list": [{
+			"contact_id": 73,
+			"phone": "13623623622",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": 1
+		}, {
+			"contact_id": 72,
+			"phone": "13652613491",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
+		}],
+		"other_contact_list": [],
+		"address_list": [],
+		"work_list": [],
+		"education_list": [{
+			"education_id": 33,
+			"type": 2,
+			"school": "当前",
+			"major": "理科",
+			"degree": 0,
+			"description": "当前",
+			"start_date": "2017-06-24",
+			"end_date": "0000-00-00"
+		}],
+		"projectList": []
+	}
 }
-at txl_memberInfo_Frame.html: 652
+at txl_memberInfo_Frame.html: 512
