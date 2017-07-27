b828c343bb7522bee1044594d5c294403d229b96
mypc <755966092@qq.com>
2017-07-27 11:41:38 +0800

    编辑求助07-27

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,28 +1,27 @@
 {
 	"code": 200,
 	"msg": "",
-	"time": 1501120111,
+	"time": 1501126208,
 	"data": {
 		"info": {
-			"supply_id": 5,
+			"demand_id": 7,
 			"user_id": 18989,
-			"title": "测试",
+			"title": "测试求助",
 			"type": 1,
-			"sub_type": 2,
 			"category_id": 1,
+			"category_name": "汽车",
 			"province": "北京市",
 			"city": "海淀区",
 			"address": "北京市海淀区中关村东路1号院-3",
-			"latitude": "40.0007760",
-			"longitude": "116.3359580",
-			"price": "2.00",
-			"unit": 0,
-			"form": 2,
-			"description": "测试",
-			"image_json": "[\"http:\\/\\/img.2017duobao.com\\/FoZLhQ1UwNUmfz3bjwZQ4Wfe7mdP\",\"http:\\/\\/img.2017duobao.com\\/Fu7-GF__HpfwdtTY2u412c1DoyiD\",\"http:\\/\\/img.2017duobao.com\\/Fh0kgyTpGT_021LnYEra-I_MM8xN\"]",
+			"latitude": "40.0007780",
+			"longitude": "116.3359650",
+			"reward": "1.00",
+			"form": 1,
+			"description": "求助",
+			"image_json": "[]",
 			"company_id": 1,
 			"company_name": ""
 		}
 	}
 }
-at supply_releaseSupply_frame.html: 1053
+at supply_releaseAsk_frame.html: 978
