1580add4131fe6cb243622a7cbfe86eae848d580
mypc <755966092@qq.com>
2017-07-28 17:05:53 +0800

    下拉刷新图片7-28

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,27 +1,106 @@
 {
-	"code": 200,
-	"msg": "",
-	"time": 1501126208,
-	"data": {
-		"info": {
-			"demand_id": 7,
-			"user_id": 18989,
-			"title": "测试求助",
-			"type": 1,
-			"category_id": 1,
-			"category_name": "汽车",
-			"province": "北京市",
-			"city": "海淀区",
-			"address": "北京市海淀区中关村东路1号院-3",
-			"latitude": "40.0007780",
-			"longitude": "116.3359650",
-			"reward": "1.00",
-			"form": 1,
-			"description": "求助",
-			"image_json": "[]",
-			"company_id": 1,
-			"company_name": ""
-		}
-	}
+	"identity": "buyer",
+	"status_info": {
+		"icon": "",
+		"text": "订单关闭",
+		"description": "订单超时，已取消，钱款将自动退回"
+	},
+	"seller_info": {
+		"user_id": 296,
+		"nickname": "袁壮",
+		"head_img_url": "http://ot0c8ontv.bkt.clouddn.com/FsSPJ_hOnHSM7UYzNar-kgEGeo6H",
+		"rongcloud_user_id": "497b966fc6d3430b39726a1e6cba7816"
+	},
+	"supply_info": {
+		"supply_id": 13,
+		"image": [],
+		"title": "测试",
+		"description": "测试",
+		"price": "0.01",
+		"unit": "幅",
+		"form": 1
+	},
+	"order_info": {
+		"order_id": 58,
+		"gen_order_id": "201707281543536986718998",
+		"amount": 1,
+		"money": "0.01",
+		"extra_money": "5.00",
+		"discount_money": "0.00",
+	"total_money": 5.01,
+		"refund_money": "0.00",
+		"create_time": "2017-07-28 15:43:53",
+		"service_time": "2017-07-29 00:00:00",
+		"reason": "超时未接单"
+	},
+	"button_info": {
+		"pay": {
+			"is_show": false,
+			"url": "/supply_order/buyerPayService"
+		},
+		"confirm_service": {
+			"is_show": false,
+			"url": "/supply_order/sellerConfirmService"
+		},
+		"refuse_service": {
+			"is_show": false,
+			"url": "/supply_order/sellerRefuseService"
+		},
+		"extend": {
+			"is_show": false,
+			"url": "/supply_order/buyerExtendService"
+		},
+		"extra": {
+			"is_show": false,
+			"url": "/supply_order/buyerPayExtraMoney"
+		},
+		"discount": {
+			"is_show": false,
+			"url": "/supply_order/sellerDiscountService"
+		},
+		"finish": {
+			"is_show": false,
+			"url": ""
+		},
+		"confirm_finish": {
+			"is_show": false,
+			"url": "/supply_order/buyerConfirmFinishService"
+		},
+		"cancel": {
+			"is_show": false,
+			"url": ""
+		},
+		"confirm_cancel": {
+			"is_show": false,
+			"url": "/supply_order/sellerConfirmCancelService"
+		},
+		"refuse_cancel": {
+			"is_show": false,
+			"url": "/supply_order/sellerRefuseCancelService"
+		},
+		"again": {
+			"is_show": false,
+			"url": "/supply_order/buyerCreateOrder"
+		},
+		"comment": {
+			"is_show": false,
+			"url": ""
+		},
+		"delete": {
+			"is_show": true,
+			"url": "/supply_order/buyerDeleteOrder",
+			"key": "删除订单"
+		},
+		"refund": {
+			"is_show": false,
+			"url": "/supply_order/sellerRefundService"
+		},
+		"buttonArr": [{
+			"is_show": true,
+			"url": "/supply_order/buyerDeleteOrder",
+			"key": "删除订单"
+		}]
+	},
+	"reason_list": []
 }
-at supply_releaseAsk_frame.html: 978
+at me_gdd_info_frame.html: 519
