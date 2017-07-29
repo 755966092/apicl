e4378ce75d16efcffe638ec578e47521ce48eede
mypc <755966092@qq.com>
2017-07-29 14:54:41 +0800

    微信支付跳转07-29

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,49 +1,17 @@
-397::: {
-	"list": [{
-		"demand_id": 7,
-		"title": "测试求助",
-		"type": 1,
-		"reward": "1.00",
-		"create_time": "2017-07-27 11:26:45",
-		"description": "求助",
-		"company_name": "",
-		"image_json": "[\"http:\\/\\/img.2017duobao.com\\/Fu7-GF__HpfwdtTY2u412c1DoyiD\",\"http:\\/\\/img.2017duobao.com\\/FoZLhQ1UwNUmfz3bjwZQ4Wfe7mdP\"]",
-		"latitude": "40.0007780",
-		"longitude": "116.3359650",
-		"nickname": "袁壮2",
-		"sex": 1,
-		"head_img_url": "http://ot0c8ontv.bkt.clouddn.com/Fmg6Owc-tHR4CjxOvQDdM6JVuki6",
-		"distance": null
-	}, {
-		"demand_id": 6,
-		"title": "测试求助22",
-		"type": 1,
-		"reward": "1.00",
-		"create_time": "2017-07-27 09:29:35",
-		"description": "测试",
-		"company_name": "",
-		"image_json": "[\"http:\\/\\/img.2017duobao.com\\/FoZLhQ1UwNUmfz3bjwZQ4Wfe7mdP\",\"http:\\/\\/img.2017duobao.com\\/Fu7-GF__HpfwdtTY2u412c1DoyiD\"]",
-		"latitude": "40.0007910",
-		"longitude": "116.3359760",
-		"nickname": "袁壮2",
-		"sex": 1,
-		"head_img_url": "http://ot0c8ontv.bkt.clouddn.com/Fmg6Owc-tHR4CjxOvQDdM6JVuki6",
-		"distance": null
-	}, {
-		"demand_id": 1,
-		"title": "孩子平时学习成绩好，想上北京的学校，求指导",
-		"type": 2,
-		"reward": "599.00",
-		"create_time": "2017-07-10 11:10:17",
-		"description": "希望能获得自主招生专家的意见，必有重谢",
-		"company_name": "中国自助招生网",
-		"image_json": "[]",
-		"latitude": "40.0007957",
-		"longitude": "116.3360833",
-		"nickname": "李彤亮",
-		"sex": 2,
-		"head_img_url": "https://wozai.tonyliangli.cn/uploads/images/default_head_img.png",
-		"distance": null
-	}]
+{
+	"code": 200,
+	"msg": "",
+	"time": 1501311111,
+	"data": {
+		"config": {
+			"appid": "wxa0dd1a8435d4bd4f",
+			"partnerid": "1485020672",
+			"prepayid": "wx20170729145152bc1b57cbae0278972953",
+			"noncestr": "597c308845ba4",
+			"timestamp": 1501311112,
+			"package": "Sign=WXPay",
+			"sign": "8BC3359A75416A8F8CDDA912297C7641"
+		}
+	}
 }
-at supply_publish_2.html: 397
+at supply_modeOfPayment.html: 329
