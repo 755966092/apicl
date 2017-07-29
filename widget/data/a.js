7b885b6bc8a9ba5274af97f57e5e6db39738c064
mypc <755966092@qq.com>
2017-07-29 15:27:59 +0800

    充值会员07-29

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,17 +1,13 @@
 {
 	"code": 200,
 	"msg": "",
-	"time": 1501311111,
+	"time": 1501312836,
 	"data": {
-		"config": {
-			"appid": "wxa0dd1a8435d4bd4f",
-			"partnerid": "1485020672",
-			"prepayid": "wx20170729145152bc1b57cbae0278972953",
-			"noncestr": "597c308845ba4",
-			"timestamp": 1501311112,
-			"package": "Sign=WXPay",
-			"sign": "8BC3359A75416A8F8CDDA912297C7641"
-		}
+		"nickname": "111",
+		"is_vip": 1,
+		"vip_end_time": "2017-08-29 15:17:44",
+		"available_assets": "670.95",
+		"list": []
 	}
 }
-at supply_modeOfPayment.html: 329
+at me_hy.html: 176
