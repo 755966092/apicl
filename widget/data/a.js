c5b46ce9210146acb5ac5f68bd48bceaeb7fcf20
mypc <755966092@qq.com>
2017-07-29 18:47:02 +0800

    修改圈子07-29

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,13 +1,27 @@
 {
-	"code": 200,
-	"msg": "",
-	"time": 1501312836,
-	"data": {
-		"nickname": "111",
-		"is_vip": 1,
-		"vip_end_time": "2017-08-29 15:17:44",
-		"available_assets": "670.95",
-		"list": []
-	}
+	"is_self": false,
+	"info": {
+		"user_info": {
+			"user_id": 1,
+			"nickname": "李彤亮",
+			"head_img_url": "http://img.2017duobao.com/FktrT_mkugDYKgmdVce94xyzLWv7",
+			"current_company": "测试",
+			"current_position": "测试",
+			"member_note": "李彤亮"
+		},
+		"topic_info": {
+			"topic_id": 8,
+			"title": "测试圈子444444444444444444444",
+			"content": "测试圈子66666",
+			"image_json": "[\"http://img.woyaogexing.com/touxiang/katong/20140110/864ea8353fe3edd3.jpg\",\"http://h.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40f0c335d94af64034f79f01985.jpg\"]",
+			"start_time": "2017-06-10 10:00:00",
+			"end_time": "2017-06-11 12:00:00",
+			"create_time": "2017-06-08 10:11:19",
+			"role": "知情人"
+		}
+	},
+	"is_collect": false,
+	"list": [],
+	"commmentLength": 0
 }
-at me_hy.html: 176
+at find_stateInfo_frame.html: 477
