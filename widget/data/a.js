117204afa87ee7e6bcae17569c54d806a0481a09
mypc <755966092@qq.com>
2017-07-30 10:11:43 +0800

    发布圈子07-30

--- widget/data/a.js
+++ widget/data/a.js
@@ -1,27 +1,53 @@
 {
-	"is_self": false,
-	"info": {
-		"user_info": {
-			"user_id": 1,
-			"nickname": "李彤亮",
-			"head_img_url": "http://img.2017duobao.com/FktrT_mkugDYKgmdVce94xyzLWv7",
-			"current_company": "测试",
-			"current_position": "测试",
-			"member_note": "李彤亮"
-		},
-		"topic_info": {
-			"topic_id": 8,
-			"title": "测试圈子444444444444444444444",
-			"content": "测试圈子66666",
-			"image_json": "[\"http://img.woyaogexing.com/touxiang/katong/20140110/864ea8353fe3edd3.jpg\",\"http://h.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40f0c335d94af64034f79f01985.jpg\"]",
-			"start_time": "2017-06-10 10:00:00",
-			"end_time": "2017-06-11 12:00:00",
-			"create_time": "2017-06-08 10:11:19",
-			"role": "知情人"
-		}
-	},
-	"is_collect": false,
-	"list": [],
-	"commmentLength": 0
+	"code": 200,
+	"msg": "",
+	"time": 1501378171,
+	"data": {
+	"user_list": [{
+			"circle_id": 5,
+			"title": "测试圈子1",
+			"description": "测试圈子1",
+			"image_url": "http://img0.imgtn.bdimg.com/it/u=646551672,3473801352&fm=26&gp=0.jpg",
+			"member_count": 9,
+			"topic_count": 0
+		}, {
+			"circle_id": 12,
+			"title": "测试微信支付",
+			"description": "支付",
+			"image_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496926338823&di=28180f83aec11ebc9bdc69c88f59201b&imgtype=0&src=http%3A%2F%2Fwww.ld12.com%2Fupimg358%2Fallimg%2Fc140921%2F14112C1C3IP-4R5S.jpg",
+			"member_count": 5,
+			"topic_count": 0
+		}, {
+			"circle_id": 13,
+			"title": "测试微信支付",
+			"description": "支付",
+			"image_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496926338823&di=28180f83aec11ebc9bdc69c88f59201b&imgtype=0&src=http%3A%2F%2Fwww.ld12.com%2Fupimg358%2Fallimg%2Fc140921%2F14112C1C3IP-4R5S.jpg",
+			"member_count": 3,
+			"topic_count": 0
+		}, {
+			"circle_id": 14,
+			"title": "测试微信支付",
+			"description": "测试",
+			"image_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496926338823&di=28180f83aec11ebc9bdc69c88f59201b&imgtype=0&src=http%3A%2F%2Fwww.ld12.com%2Fupimg358%2Fallimg%2Fc140921%2F14112C1C3IP-4R5S.jpg",
+			"member_count": 3,
+			"topic_count": 0
+		}],
+		
+		"other_list": [{
+			"circle_id": 2,
+			"title": "测试圈子1",
+			"description": "测试圈子1",
+			"image_url": "http://img0.imgtn.bdimg.com/it/u=646551672,3473801352&fm=26&gp=0.jpg",
+			"member_count": 5,
+			"topic_count": 0
+		}, {
+			"circle_id": 3,
+			"title": "测试圈子2",
+			"description": "测试圈子2",
+			"image_url": "http://img5.duitang.com/uploads/item/201508/31/20150831220841_escLY.thumb.224_0.jpeg",
+			"member_count": 1,
+			"topic_count": 0
+		}]
+	}
 }
-at find_stateInfo_frame.html: 477
+at find_publishpro_frame_selCircle.html: 32
