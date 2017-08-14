48c37f943517ac34b50534f2a76f85779c8dc2a1
mypc <755966092@qq.com>
2017-08-14 17:12:30 +0800

    供求分开筛选08-14

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,119 +1,52 @@
-{
-	"identity": "buyer",
-	"status_info": {
-		"icon": "FINISH",
-		"text": "交易成功",
-		"description": "钱款已支付给对方，如有异议请与对方联系"
-	},
-	"seller_info": {
-		"user_id": 837,
-		"nickname": "测试2",
-		"head_img_url": "http://img.iinnet.com/system/default-head-img.png",
-		"rongcloud_user_id": "1c007ebdf083d6af7ffe5fe4e0cfb6e5"
-	},
-	"supply_info": {
-		"supply_id": 8,
-		"image": [],
-		"title": "供应测试",
-		"description": "测试",
-		"price": "0.01",
-		"unit": "幅",
-		"form": 1
-	},
-	"order_info": {
-		"order_id": 57,
-		"gen_order_id": "201708141104434895830819",
-		"amount": 1,
-		"money": "0.01",
-		"extra_money": "0.00",
-		"discount_money": "0.00",
-		"total_money": 0.01,
-		"refund_money": "0.01",
-		"create_time": "2017-08-14 11:04:43",
-		"service_time": "2017-08-19 14:04:00",
-		"note": "",
-	"reason": "",
-		"status": 7
-	},
-	"button_info": {
-		"pay": {
-			"is_show": false,
-			"url": "/supply_order/buyerPayService"
-		},
-		"confirm_service": {
-			"is_show": false,
-			"url": "/supply_order/sellerConfirmService"
-		},
-		"refuse_service": {
-			"is_show": false,
-			"url": "/supply_order/sellerRefuseService"
-		},
-		"extend": {
-			"is_show": false,
-			"url": "/supply_order/buyerExtendService"
+获取线上信息::: {
+	"code": 200,
+	"msg": "",
+	"time": 1502693642,
+	"data": {
+		"info": {
+			"user_id": 376,
+			"nickname": "a杨总",
+			"real_name": "",
+			"sex": 2,
+			"head_img_url": "http://img.iinnet.com/FgZVDPywjcNP6rCkOS2ptbuwztX-",
+			"hometown": null,
+			"birthday": null,
+			"current_company": null,
+			"current_position": null,
+			"current_degree": null,
+			"current_contact": "7631313"
 		},
 		"extra": {
-			"is_show": false,
-			"url": "/supply_order/buyerPayExtraMoney"
+			"extra_id": 4,
+			"remark_user_id": 494,
+			"content": null,
+			"image_url": null
 		},
-		"discount": {
-			"is_show": false,
-			"url": "/supply_order/sellerDiscountService"
-		},
-		"finish": {
-			"is_show": false,
-			"url": ""
-		},
-		"confirm_finish": {
-			"is_show": false,
-			"url": "/supply_order/buyerConfirmFinishService"
-		},
-		"cancel": {
-			"is_show": false,
-			"url": ""
-		},
-		"confirm_cancel": {
-			"is_show": false,
-			"url": "/supply_order/sellerConfirmCancelService"
-		},
-		"refuse_cancel": {
-			"is_show": false,
-			"url": "/supply_order/sellerRefuseCancelService"
-		},
-		"again": {
-			"is_show": true,
-			"url": "/supply_order/buyerCreateOrder",
-			"key": "再来一单"
-		},
-		"comment": {
-			"is_show": true,
-			"url": "/supply_order/buyerCommentService",
-			"key": "评价订单"
-		},
-		"delete": {
-			"is_show": true,
-			"url": "/supply_order/buyerDeleteOrder",
-			"key": "删除订单"
-		},
-		"refund": {
-			"is_show": false,
-			"url": "/supply_order/sellerRefundService"
-		},
-		"buttonArr": [{
-			"is_show": true,
-			"url": "/supply_order/buyerCreateOrder",
-			"key": "再来一单"
+		"status": [],
+		"contact_list": [{
+			"contact_id": 27,
+			"phone": "7631313",
+			"type": 2,
+			"sub_type": 1,
+			"is_default": 1
 		}, {
-			"is_show": true,
-			"url": "/supply_order/buyerCommentService",
-			"key": "评价订单"
+			"contact_id": 23,
+			"phone": "13260337521",
+			"type": 1,
+			"sub_type": 1,
+			"is_default": -1
 		}, {
-			"is_show": true,
-			"url": "/supply_order/buyerDeleteOrder",
-			"key": "删除订单"
-		}]
-	},
-	"reason_list": [],
-	"btnurl": "/supply_order/buyerCommentService"
+			"contact_id": 21,
+			"phone": "0107631313",
+			"type": 2,
+			"sub_type": 1,
+			"is_default": -1
+		}],
+		"other_contact_list": [],
+		"address_list": [],
+		"work_list": [],
+		"education_list": [],
+		"project_list": []
+	}
 }
-at me_gdd_info_frame.html: 637
+at txl_memberInfo_Frame.html: 647
