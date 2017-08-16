a59000e2abaff38cfedde55e99215392de7f3d2c
mypc <755966092@qq.com>
2017-08-16 15:10:41 +0800

    用户信息对比08-16

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,65 +1,29 @@
-463::: [{
-	"draft": "",
-	"sentStatus": "SENT",
-	"sentTime": 1502847853111,
-	"latestMessage": {
-		"extra": "",
-		"text": "[吐]"
-	},
-	"conversationType": "PRIVATE",
-	"receivedStatus": "READ",
-	"conversationTitle": "",
-	"objectName": "RC:TxtMsg",
-	"senderUserId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
-	"unreadMessageCount": 0,
-	"receivedTime": 1502847853026,
-	"isTop": false,
-	"latestMessageId": 47,
-	"targetId": "759d2765861e5ba5f4b8e576bfeaee33",
-	"time": {
-		"sencond": 13,
-		"minute": 44,
-		"hour": 9,
-		"year": 2017,
-		"month": 7,
-		"day": 3
-	},
-	"url": "http://img.iinnet.com/system/default-head-img.png",
-	"nickname": "测试13260",
-	"user_id": 909,
+数据库信息::: {
+	"user_id": "837",
+	"nickname": "测试2",
+	"real_name": "姓名",
+	"head_img_url": "http://img.iinnet.com/system/default-head-img.png",
+	"current_position": "",
+	"current_company": "",
+	"current_degree": "",
+	"current_contact": "",
+	"titleSize": "15",
+	"title": "测试2",
+	"img": "http://img.iinnet.com/system/default-head-img.png",
+	"flag": "C",
+	"litter": "CS2",
+	"type": "4",
+	"sex": "2",
 	"remark_nickname": "",
-	"remark_head_img_url": ""
-}, {
-	"draft": "",
-	"sentStatus": "SENT",
-	"sentTime": 1502787080983,
-	"latestMessage": {
-		"voicePath": "/var/mobile/Containers/Data/Application/5FE41D9C-EDF0-4F1F-B9AC-FC7EB75D62D1/Documents/a9fc444a4ad3729bf928febcaee81afefc4e64d3",
-		"duration": 3,
-		"extra": "",
-		"text": "[语音]"
-	},
-	"conversationType": "PRIVATE",
-	"receivedStatus": "READ",
-	"conversationTitle": "",
-	"objectName": "RC:VcMsg",
-	"senderUserId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
-	"unreadMessageCount": 0,
-	"receivedTime": 1502787080895,
-	"isTop": false,
-	"latestMessageId": 7,
-	"targetId": "a6d8bbb686fa029e2be8f5d93ee87215",
-	"time": {
-		"sencond": 20,
-		"minute": 51,
-		"hour": 16,
-		"year": 2017,
-		"month": 7,
-		"day": 2
-	},
-	"url": "http://img.iinnet.com/FhVIRSYCgWN9r7QvEtiDFU0fJ3Hi",
-	"nickname": "测试1",
-	"user_id": 376,
-	"remark_nickname": "备注昵称",
-	"remark_head_img_url": "http://img.iinnet.com/FhVIRSYCgWN9r7QvEtiDFU0fJ3Hi"
-}]
+	"remark_real_name": "",
+	"remark_head_img_url": "",
+	"remark_sex": "",
+	"remark_birthday": "",
+	"remark_hometown": "",
+	"remark_current_company": "",
+	"remark_current_position": "",
+	"remark_current_degree": "",
+	"remark_current_contact": "",
+	"current_status": ""
+}
+at txl_memberInfo_Frame.html: 662
