deac27b79cc963da93562002878189706a08d90e
mypc <755966092@qq.com>
2017-08-16 09:55:39 +0800

    语音聊天08-16

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,28 +1,65 @@
-258::: {
-	"value": {
-		"targetId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
-		"msgContent": {
-			"result": {
-				"left": 0,
-				"message": {
-					"content": {
-						"duration": 3,
-						"extra": "",
-						"voicePath": "/data/user/0/com.iinnet.apicloud/files/759d2765861e5ba5f4b8e576bfeaee33/voice/20.amr"
-					},
-					"conversationType": "PRIVATE",
-					"extra": "",
-					"messageDirection": "RECEIVE",
-					"messageId": 20,
-					"objectName": "RC:VcMsg",
-					"receivedTime": 1502791126712,
-					"senderUserId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
-					"sentStatus": "SENT",
-					"sentTime": 1502791126436,
-					"targetId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5"
-				}
-			}
-		}
-	}
-}
-at chatContent.html: 293
+463::: [{
+	"draft": "",
+	"sentStatus": "SENT",
+	"sentTime": 1502847853111,
+	"latestMessage": {
+		"extra": "",
+		"text": "[吐]"
+	},
+	"conversationType": "PRIVATE",
+	"receivedStatus": "READ",
+	"conversationTitle": "",
+	"objectName": "RC:TxtMsg",
+	"senderUserId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
+	"unreadMessageCount": 0,
+	"receivedTime": 1502847853026,
+	"isTop": false,
+	"latestMessageId": 47,
+	"targetId": "759d2765861e5ba5f4b8e576bfeaee33",
+	"time": {
+		"sencond": 13,
+		"minute": 44,
+		"hour": 9,
+		"year": 2017,
+		"month": 7,
+		"day": 3
+	},
+	"url": "http://img.iinnet.com/system/default-head-img.png",
+	"nickname": "测试13260",
+	"user_id": 909,
+	"remark_nickname": "",
+	"remark_head_img_url": ""
+}, {
+	"draft": "",
+	"sentStatus": "SENT",
+	"sentTime": 1502787080983,
+	"latestMessage": {
+		"voicePath": "/var/mobile/Containers/Data/Application/5FE41D9C-EDF0-4F1F-B9AC-FC7EB75D62D1/Documents/a9fc444a4ad3729bf928febcaee81afefc4e64d3",
+		"duration": 3,
+		"extra": "",
+		"text": "[语音]"
+	},
+	"conversationType": "PRIVATE",
+	"receivedStatus": "READ",
+	"conversationTitle": "",
+	"objectName": "RC:VcMsg",
+	"senderUserId": "1c007ebdf083d6af7ffe5fe4e0cfb6e5",
+	"unreadMessageCount": 0,
+	"receivedTime": 1502787080895,
+	"isTop": false,
+	"latestMessageId": 7,
+	"targetId": "a6d8bbb686fa029e2be8f5d93ee87215",
+	"time": {
+		"sencond": 20,
+		"minute": 51,
+		"hour": 16,
+		"year": 2017,
+		"month": 7,
+		"day": 2
+	},
+	"url": "http://img.iinnet.com/FhVIRSYCgWN9r7QvEtiDFU0fJ3Hi",
+	"nickname": "测试1",
+	"user_id": 376,
+	"remark_nickname": "备注昵称",
+	"remark_head_img_url": "http://img.iinnet.com/FhVIRSYCgWN9r7QvEtiDFU0fJ3Hi"
+}]
