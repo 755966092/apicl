15e166f3902fe9421d2b50f43c442d52a9c3b3db
mypc <755966092@qq.com>
2017-08-22 18:57:42 +0800

    解决联系人列表问题08-21

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,57 +1,8 @@
-{
-	"code": 200,
-	"msg": "",
-	"time": 1503285804,
-	"data": {
-		"list": [{
-			"demand_id": 10,
-			"title": "测试求",
-			"type": 1,
-		"reward": "0.01",
-			"create_time": "2017-08-16 15:47:22",
-			"description": "测试",
-			"company_name": "",
-			"image_json": "[]",
-			"latitude": "40.0007830",
-			"longitude": "116.3359370",
-			"user_id": 909,
-			"nickname": "测试13260",
-			"sex": null,
-			"head_img_url": "http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z",
-			"distance": null
-		}, {
-			"demand_id": 9,
-			"title": "测试",
-			"type": 1,
-			"reward": "0.01",
-			"create_time": "2017-08-16 15:41:58",
-			"description": "测试",
-			"company_name": "",
-			"image_json": "[]",
-			"latitude": "40.0008051",
-			"longitude": "116.3361201",
-			"user_id": 837,
-			"nickname": "测试2",
-			"sex": 2,
-			"head_img_url": "http://img.iinnet.com/system/default-head-img.png",
-			"distance": null
-		}, {
-			"demand_id": 5,
-			"title": "测试推送",
-			"type": 1,
-			"reward": "0.01",
-			"create_time": "2017-08-14 14:06:25",
-			"description": "测试",
-			"company_name": "",
-			"image_json": "[]",
-			"latitude": "40.0007690",
-			"longitude": "116.3359470",
-			"user_id": 376,
-			"nickname": "测试1",
-			"sex": null,
-			"head_img_url": "http://img.iinnet.com/FhVIRSYCgWN9r7QvEtiDFU0fJ3Hi",
-			"distance": null
-		}]
-	}
-}
-at supply_searchSupplyFruit_frame.html: 404
+$(document).ready(function($) {
+	$('.scrollTo').click(function() {
+		$('html,body').animate({
+			scrollTop: $($(this).attr("href")).offset().top
+		}, 800);
+		return false;
+	});
+});
