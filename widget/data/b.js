8b9f721e3c9f53093c75c20f4b3bd09e69ab0f0e
yzz <755966092@qq.com>
2017-09-05 19:17:26 +0800

    聊天列表修复09-06

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,30 +1,17 @@
-400: {
-    "code": 200,
-    "data": {
-        "list": [{
-            "remark_hometown": null,
-            "remark_current_contact": "",
-            "birthday": null,
-            "remark_nickname": null,
-            "remark_birthday": null,
-            "user_id": 837,
-            "nickname": "测试2",
-            "remark_head_img_url": null,
-            "sex": 2,
-            "current_contact": "",
-            "remark_real_name": null,
-            "head_img_url": "http://img.iinnet.com/Fmz4QrS442UQOgkQVn2t4g25xy4t",
-            "current_degree": "",
-            "remark_sex": null,
-            "current_company": "测试工作",
-            "remark_current_company": null,
-            "remark_current_position": null,
-            "remark_current_degree": null,
-            "current_position": "备注",
-            "real_name": "姓名",
-            "hometown": ""
-        }]
+历史消息1 {
+    "content": {
+        "extra": "",
+        "text": "[撇嘴][撇嘴]"
     },
-    "msg": "",
-    "time": 1504593350
+    "conversationType": "PRIVATE",
+    "extra": "",
+    "messageDirection": "SEND",
+    "messageId": 1,
+    "objectName": "RC:TxtMsg",
+    "receivedTime": 1504579599768,
+    "senderUserId": "47d2b511386521e8235274b1287e5525",
+    "sentStatus": "SENT",
+    "sentTime": 1504579601497,
+    "targetId": "759d2765861e5ba5f4b8e576bfeaee33"
 }
+at chatContent.html: 1069
