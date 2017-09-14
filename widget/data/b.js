ec09a3e0965801e264839ee3532ef7dcc028ca8a
Mr-Yuan <755966092@qq.com>
2017-09-14 16:10:42 +0800

    测试通讯录列表09-14

---
+++ widget/data/b.js
@@ -0,0 +1,8 @@
+{"user_id":"494",
+"title":"11a杨总",
+"img":"http://img.iinnet.com/system/default-head-img.png",
+"subTitle":"",
+"current_contact":"18763830562",
+"flag":"1",
+"type":"3",
+"initial":"#"
