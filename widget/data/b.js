0435649708e09788870d3d7102a95edcdd252ad8
Mr-Yuan <755966092@qq.com>
2017-10-11 15:05:14 +0800

    修复页面bug010-11

--- widget/data/b.js
+++ widget/data/b.js
@@ -1,20 +1,74 @@
 {
-    "left": 0,
-    "message": {
-        "content": {
-            "extra": "",
-            "imageUrl": "https://rongcloud-image.cn.ronghub.com/image_jpeg__RC-2017-09-29_3278_15066…oken=livk5rb3__JZjCtEiMxXpQ8QscLxbNLehwhHySnX:KFPsBhGJNoLgDdcCH33wHaR81KA=",
-            "thumbPath": "/data/user/0/com.iinnet.apicloud/files/47d2b511386521e8235274b1287e5525/image/thumbnail/6.jpg"
+    "code": 200,
+    "msg": "",
+    "time": "1507704689",
+    "data": {
+        "identity": "host",
+        "status_info": {
+            "icon": "CLOSE",
+            "text": "订单关闭",
+            "description": "订单超时，已取消"
         },
-        "conversationType": "PRIVATE",
-        "extra": "",
-        "messageDirection": "RECEIVE",
-        "messageId": 6,
-        "objectName": "RC:ImgMsg",
-        "receivedTime": 1506676679171,
-        "senderUserId": "759d2765861e5ba5f4b8e576bfeaee33",
-        "sentStatus": "SENT",
-        "sentTime": 1506676679046,
-        "targetId": "759d2765861e5ba5f4b8e576bfeaee33"
+        "helper_info": {
+            "user_id": 909,
+            "nickname": "测试13260",
+            "head_img_url": "http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z",
+            "rongcloud_user_id": "759d2765861e5ba5f4b8e576bfeaee33"
+        },
+        "demand_info": {
+            "demand_id": 64,
+            "image": [],
+            "title": "测试19",
+            "description": "测试19",
+            "form": 2,
+            "reward": "1.00"
+        },
+        "order_info": {
+            "order_id": 23,
+            "gen_order_id": "201709071039408648461020",
+            "amount": 1,
+            "money": "1.00",
+            "create_time": "2017-09-07 10:39:40",
+            "note": "",
+            "reason": "超时未接单"
+        },
+        "button_info": {
+            "cancel_order": {
+                "is_show": false,
+                "url": "/demand_order/helperCancelOrder"
+            },
+            "confirm_help": {
+                "is_show": false,
+                "url": "/demand_order/hostConfirmHelp"
+            },
+            "refuse_help": {
+                "is_show": false,
+                "url": "/demand_order/hostRefuseHelp"
+            },
+            "finish": {
+                "is_show": false,
+                "url": ""
+            },
+            "confirm_finish": {
+                "is_show": false,
+                "url": "/demand_order/hostConfirmFinishHelp"
+            },
+            "cancel": {
+                "is_show": false,
+                "url": ""
+            },
+            "comment": {
+                "is_show": false,
+                "url": ""
+            },
+            "delete": {
+                "is_show": true,
+                "url": "/demand_order/hostDeleteOrder"
+            }
+        },
+        "reason_list": []
     }
 }
+at
+me_qdd_info_frame.html:
+    512
