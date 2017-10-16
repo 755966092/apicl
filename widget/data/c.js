a01ccd3985db98c0586715fa704ac4860268b746
Mr-Yuan <755966092@qq.com>
2017-10-16 18:01:48 +0800

    附近的人功能10-16

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,323 +1,45 @@
 {
 	"code": 200,
-	"data": {
-		"user_info": {
-			"909": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 909,
-				"nickname": "测试13260",
-				"head_img_url": "http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z"
-			},
-			"20240": {
-				"remark_nickname": "公司—陈佳伟",
-				"remark_head_img_url": "",
-				"user_id": 20240,
-				"nickname": "陈佳伟",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			},
-			"94189": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 94189,
-				"nickname": "王雪松",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			}
-		},
-		"relationship_info": [
-			["909", "20240"],
-			["20240", "909"],
-			["20240", "94189"],
-			["94189", "20240"]
-		]
-	},
 	"msg": "",
-	"time": "1507880126"
-}
-
-
-
-{
-	"data": [{
-		"name": "909",
-		"symbol": "image:/http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z"
-	}, {
-		"name": "20240",
-		"symbol": "image:/http://img.iinnet.com/system/default-head-img.png"
-	}, {
-		"name": "94189",
-		"symbol": "image:/http://img.iinnet.com/system/default-head-img.png"
-	}],
-	"links": [{
-		"source": "909",
-		"target": "20240"
-	}, {
-		"source": "20240",
-		"target": "909"
-	}, {
-		"source": "20240",
-		"target": "94189"
-	}, {
-		"source": "94189",
-		"target": "20240"
-	}]
-}
-
-{
-	"code": 200,
+	"time": "1508146926",
 	"data": {
-		"user_info": {
-			"228": {
-				"remark_nickname": "Boweniac",
-				"remark_head_img_url": "http://img.iinnet.com/system/default-head-img.png",
-				"user_id": 228,
-				"nickname": "Boweniac",
-				"head_img_url": "http://img.iinnet.com/Fl-y1Auz3v2NQQw2XE2tzPFK6UWe"
-			},
-			"909": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 909,
-				"nickname": "测试13260",
-				"head_img_url": "http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z"
-			},
-			"970": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 970,
-				"nickname": "东施西施",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			}
-		},
-		"relationship_info": [
-			["909", "228"],
-			["228", "909"],
-			["228", "970"],
-			["970", "228"]
-		]
-	},
-	"msg": "",
-	"time": "1507881990"
+		"list": [{
+			"user_id": 3,
+			"nickname": "李彤亮",
+			"head_img_url": "http://img.iinnet.com/Fv1kOKTOdp7hvbVu2oDNlehnBB3V",
+			"sex": 1,
+			"age": 25,
+			"hometown": "山东-青岛",
+			"current_company": "第一学教育科技（北京）有限公司",
+			"current_position": "后端技术研发",
+			"current_degree": "",
+			"distance": 1.01,
+			"address": "清华科技园创业大厦"
+		}, {
+			"user_id": 9969,
+			"nickname": "大中小18765",
+			"head_img_url": "http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh",
+			"sex": 1,
+			"age": 23,
+			"hometown": "山东-菏泽",
+			"current_company": "第一学",
+			"current_position": "前端开发",
+			"current_degree": "",
+			"distance": 7.61,
+			"address": "北京市昌平区建材城西路辅路"
+		}, {
+			"user_id": 18,
+			"nickname": "姜艳",
+			"head_img_url": "http://img.iinnet.com/system/default-head-img.png",
+			"sex": 2,
+			"age": 24,
+			"hometown": "江苏-扬州",
+			"current_company": "",
+			"current_position": "",
+			"current_degree": "",
+			"distance": 12.51,
+			"address": "天安门"
+		}]
+	}
 }
-
-
-{
-	"code": 200,
-	"data": {
-		"user_info": {
-			"3": {
-				"remark_nickname": "测试",
-				"remark_head_img_url": "http://img.iinnet.com/FudOQo_IDY9fUAkYwA4vRuO37bIQ",
-				"user_id": 3,
-				"nickname": "李彤亮",
-				"head_img_url": "http://img.iinnet.com/Fv1kOKTOdp7hvbVu2oDNlehnBB3V"
-			},
-			"18": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 18,
-				"nickname": "姜艳",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			},
-			"228": {
-				"remark_nickname": "Boweniac",
-				"remark_head_img_url": "http://img.iinnet.com/system/default-head-img.png",
-				"user_id": 228,
-				"nickname": "Boweniac",
-				"head_img_url": "http://img.iinnet.com/Fl-y1Auz3v2NQQw2XE2tzPFK6UWe"
-			},
-			"231": {
-				"remark_nickname": "公司—张建强",
-				"remark_head_img_url": "",
-				"user_id": 231,
-				"nickname": "gift",
-				"head_img_url": "http://img.iinnet.com/FkTAuPiA5RHzv4VyVtn_ijtAdHks"
-			},
-			"236": {
-				"remark_nickname": "杨慧",
-				"remark_head_img_url": "",
-				"user_id": 236,
-				"nickname": "Una",
-				"head_img_url": "http://img.iinnet.com/FtiKpkN07pcKR-f77pown3PgTVy9"
-			},
-			"877": {
-				"remark_nickname": "公司—王松柏",
-				"remark_head_img_url": "",
-				"user_id": 877,
-				"nickname": "松柏",
-				"head_img_url": "http://img.iinnet.com/FvqXglBNsdVToJgzX0UKhsPYHW7a"
-			},
-			"909": {
-				"remark_nickname": null,
-				"remark_head_img_url": null,
-				"user_id": 909,
-				"nickname": "测试13260",
-				"head_img_url": "http://img.iinnet.com/FlcfxZr8WXKbW73wfy4tn7LNU-2Z"
-			},
-			"1026": {
-				"remark_nickname": "公司—孙为超",
-				"remark_head_img_url": "",
-				"user_id": 1026,
-				"nickname": "hatter",
-				"head_img_url": "http://img.iinnet.com/FoyvDwpaOeufXCJ-BQFQElKGlQc7"
-			},
-			"1027": {
-				"remark_nickname": "公司—刘隆昌",
-				"remark_head_img_url": "",
-				"user_id": 1027,
-				"nickname": "清奇",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			},
-			"1029": {
-				"remark_nickname": "公司—王文培",
-				"remark_head_img_url": "",
-				"user_id": 1029,
-				"nickname": "老土",
-				"head_img_url": "http://img.iinnet.com/FtnTrOf6LgiqLJ_NG-soghj2YkiB"
-			},
-			"1885": {
-				"remark_nickname": "公司—赵老师",
-				"remark_head_img_url": "",
-				"user_id": 1885,
-				"nickname": "佛易道科",
-				"head_img_url": "http://img.iinnet.com/FmDMt6z5XeEC4OPkS7bFX9_ZnUdX"
-			},
-			"10309": {
-				"remark_nickname": "公司—仲敏",
-				"remark_head_img_url": "",
-				"user_id": 10309,
-				"nickname": "黑娃",
-				"head_img_url": "http://img.iinnet.com/FhWFGhDhjiwx9f_tu_zea4ozEMSw"
-			},
-			"20240": {
-				"remark_nickname": "公司—陈佳伟",
-				"remark_head_img_url": "",
-				"user_id": 20240,
-				"nickname": "陈佳伟",
-				"head_img_url": "http://img.iinnet.com/system/default-head-img.png"
-			}
-		},
-		"relationship_info": [
-			["909", "1026"],
-			["909", "877"],
-			["909", "228"],
-			["909", "231"],
-			["909", "1027"],
-			["909", "10309"],
-			["909", "1029"],
-			["909", "20240"],
-			["909", "1885"],
-			["909", "236"],
-			["3", "909"],
-			["228", "909"],
-			["20240", "909"],
-			["236", "909"],
-			["231", "909"],
-			["1027", "909"],
-			["1029", "909"],
-			["1026", "909"],
-			["877", "909"],
-			["10309", "909"],
-			["228", "20240"],
-			["228", "10309"],
-			["228", "231"],
-			["228", "1026"],
-			["228", "1885"],
-			["228", "1027"],
-			["228", "1029"],
-			["228", "877"],
-			["228", "236"],
-			["228", "3"],
-			["3", "228"],
-			["20240", "228"],
-			["236", "228"],
-			["231", "228"],
-			["1027", "228"],
-			["1029", "228"],
-			["1026", "228"],
-			["877", "228"],
-			["10309", "228"],
-			["10309", "236"],
-			["10309", "3"],
-			["10309", "877"],
-			["10309", "1029"],
-			["10309", "231"],
-			["10309", "20240"],
-			["10309", "1026"],
-			["10309", "1027"],
-			["3", "10309"],
-			["20240", "10309"],
-			["236", "10309"],
-			["231", "10309"],
-			["1027", "10309"],
-			["1029", "10309"],
-			["1026", "10309"],
-			["877", "10309"],
-			["877", "236"],
-			["877", "3"],
-			["877", "20240"],
-			["877", "1026"],
-			["877", "1027"],
-			["877", "1029"],
-			["877", "231"],
-			["3", "877"],
-			["20240", "877"],
-			["236", "877"],
-			["231", "877"],
-			["1027", "877"],
-			["1029", "877"],
-			["1026", "877"],
-			["1885", "877"],
-			["20240", "1027"],
-			["20240", "231"],
-			["20240", "1026"],
-			["20240", "1029"],
-			["20240", "3"],
-			["20240", "236"],
-			["3", "20240"],
-			["236", "20240"],
-			["231", "20240"],
-			["1027", "20240"],
-			["1029", "20240"],
-			["1026", "20240"],
-			["1027", "236"],
-			["1027", "1029"],
-			["1027", "231"],
-			["1027", "3"],
-			["1027", "1026"],
-			["3", "1027"],
-			["236", "1027"],
-			["231", "1027"],
-			["1029", "1027"],
-			["1026", "1027"],
-			["1026", "231"],
-			["1026", "1029"],
-			["1026", "3"],
-			["1026", "236"],
-			["3", "1026"],
-			["236", "1026"],
-			["231", "1026"],
-			["1029", "1026"],
-			["1885", "1026"],
-			["236", "1029"],
-			["236", "3"],
-			["236", "231"],
-			["3", "236"],
-			["231", "236"],
-			["1029", "236"],
-			["1885", "236"],
-			["1029", "3"],
-			["1029", "231"],
-			["3", "1029"],
-			["231", "1029"],
-			["1885", "1029"],
-			["3", "18"],
-			["3", "231"],
-			["231", "3"],
-			["1885", "3"],
-			["18", "3"]
-		]
-	},
-	"msg": "",
-	"time": "1507882559"
-}
+at find_peopleAround_frame.html: 159
