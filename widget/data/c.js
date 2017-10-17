e0bcb6fb7e9d6d09c36c5e96cc14a0fe9e616e09
Mr-Yuan <755966092@qq.com>
2017-10-17 18:05:52 +0800

    我收藏的圈子动态10-17

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,130 +1,45 @@
 {
 	"code": 200,
 	"msg": "",
-	"time": "1508230574",
+	"time": "1508234231",
 	"data": {
-		"is_member": true,
-		"is_vip": false,
-		"none_vip_list_count": 5,
-		"vip_list_count": 10,
 		"list": [{
+			"topic_id": 10,
+			"title": "欢迎大家分享技术",
+			"image_json": "[]",
+			"create_time": "2017-09-12 17:26:29",
+			"start_time": "2017-09-12 00:00:00",
+			"user_id": 3,
+			"nickname": "李彤亮",
+			"head_img_url": "http://img.iinnet.com/Fv1kOKTOdp7hvbVu2oDNlehnBB3V",
+			"current_company": "第一学教育科技（北京）有限公司",
+			"current_position": "后端技术研发",
+			"role": 3
+		}, {
+			"topic_id": 14,
+			"title": "自主招生问题",
+			"image_json": "[\"http:\\/\\/img.iinnet.com\\/FkYhKHN_-XkFgFFW3r8wVRs3-8LJ\"]",
+			"create_time": "2017-09-12 20:16:26",
+			"start_time": "2017-09-10 00:00:00",
+			"user_id": 877,
+			"nickname": "松柏",
+			"head_img_url": "http://img.iinnet.com/FvqXglBNsdVToJgzX0UKhsPYHW7a",
+			"current_company": "",
+			"current_position": "",
+			"role": 4
+		}, {
 			"topic_id": 7,
-			"circle_id": 1,
+			"title": "大家好",
+			"image_json": "[]",
+			"create_time": "2017-08-27 09:16:40",
+			"start_time": "2017-08-27 00:00:00",
 			"user_id": 204,
 			"nickname": "佛易道科",
 			"head_img_url": "http://img.iinnet.com/Fq_SwUAj6_nn-cl3NOFcOSwfQApE",
-			"role": "知情人",
-			"role_name": "关系人",
-			"content": "大家好",
-			"image_json": [],
-			"start_time": "2017-08-27 00:00:00",
-			"end_time": "2017-09-02 00:00:00",
-			"create_time": "2017-08-27 09:16:40",
-			"is_self": "-1",
-			"member_note": "我在用户交流圈",
-			"comment_list": [{
-				"user_id": 505,
-				"nickname": "尹中华",
-				"comment_id": 3,
-				"create_time": "2017-08-27 09:23:09",
-				"content": "测试通过了？",
-				"root": 1,
-				"comment_user_id": null,
-				"comment_nickname": null,
-				"authority": 1
-			}]
-		}, {
-			"topic_id": 1,
-			"circle_id": 1,
-			"user_id": 505,
-			"nickname": "尹中华",
-			"head_img_url": "http://img.iinnet.com/FrT9kKWQnre3tTRGUpmmO0l__6Xx",
-			"role": "知情人",
-			"role_name": "关系人",
-			"content": "求联系方式",
-			"image_json": ["http://img.iinnet.com/Frsb9yimkPhm0xSVZ-xZ2iycKa0H"],
-			"start_time": "2017-08-12 00:00:00",
-			"end_time": "2017-08-13 00:00:00",
-			"create_time": "2017-08-12 11:54:29",
-			"is_self": "-1",
-			"member_note": "",
-			"comment_list": [{
-				"user_id": 505,
-				"nickname": "尹中华",
-				"comment_id": 1,
-				"create_time": "2017-08-12 11:57:04",
-				"content": "终于让你看到消息啦，我是误打误撞发现这里的，感觉像发现了宝贝，如果方便请加我微信 crossey ",
-				"root": 1,
-				"comment_user_id": null,
-				"comment_nickname": null,
-				"authority": 1
-			}]
+			"current_company": "",
+			"current_position": "",
+			"role": 3
 		}]
 	}
 }
-at find_dynamicState_frame.html: 398 
-{
-	"code": 200,
-	"msg": "",
-	"time": "1508230567",
-	"data": {
-		"is_member": true,
-		"is_vip": false,
-		"none_vip_list_count": 5,
-		"vip_list_count": 10,
-		"list": [{
-			"topic_id": 7,
-			"circle_id": 1,
-			"user_id": 204,
-			"nickname": "佛易道科",
-			"head_img_url": "http://img.iinnet.com/Fq_SwUAj6_nn-cl3NOFcOSwfQApE",
-			"role": "知情人",
-			"role_name": "关系人",
-			"content": "大家好",
-			"image_json": [],
-			"start_time": "2017-08-27 00:00:00",
-			"end_time": "2017-09-02 00:00:00",
-			"create_time": "2017-08-27 09:16:40",
-			"is_self": "-1",
-			"member_note": "我在用户交流圈",
-			"comment_list": [{
-				"user_id": 505,
-				"nickname": "尹中华",
-				"comment_id": 3,
-				"create_time": "2017-08-27 09:23:09",
-				"content": "测试通过了？",
-				"root": 1,
-				"comment_user_id": null,
-				"comment_nickname": null,
-				"authority": 1
-			}]
-		}, {
-			"topic_id": 1,
-			"circle_id": 1,
-			"user_id": 505,
-			"nickname": "尹中华",
-			"head_img_url": "http://img.iinnet.com/FrT9kKWQnre3tTRGUpmmO0l__6Xx",
-			"role": "知情人",
-			"role_name": "关系人",
-			"content": "求联系方式",
-			"image_json": ["http://img.iinnet.com/Frsb9yimkPhm0xSVZ-xZ2iycKa0H"],
-			"start_time": "2017-08-12 00:00:00",
-			"end_time": "2017-08-13 00:00:00",
-			"create_time": "2017-08-12 11:54:29",
-			"is_self": "-1",
-			"member_note": "",
-			"comment_list": [{
-				"user_id": 505,
-				"nickname": "尹中华",
-				"comment_id": 1,
-				"create_time": "2017-08-12 11:57:04",
-				"content": "终于让你看到消息啦，我是误打误撞发现这里的，感觉像发现了宝贝，如果方便请加我微信 crossey ",
-				"root": 1,
-				"comment_user_id": null,
-				"comment_nickname": null,
-				"authority": 1
-			}]
-		}]
-	}
-}
-at find_dynamicState_frame.html: 397
+at me_sc_publish_3.html: 189
