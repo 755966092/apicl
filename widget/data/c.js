858a67a9989360b25d19302915f30a82d3745d69
Mr-Yuan <755966092@qq.com>
2017-10-18 18:50:27 +0800

    收藏页面bug10-18

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,1 +1,44 @@
-INSERT OR REPLACE INTO tb_chat_47d2b511386521e8235274b1287e5525(messageId, receivedTime, objectName, messageDirection, content_extra, content_text, head_img_url, filePath, duration, thumbPath) VALUES (9,"1508313244781","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(8,"1508313157312","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(7,"1508313061487","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(6,"1508312898639","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(5,"1508312590524","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(4,"1508312534684","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(3,"1508312040491","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(2,"1508311931579","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","",""),(1,"1508311704348","RC:TxtMsg","RECEIVE","{type:4,reference_id:9969}","我通过了你的朋友验证请求，现在我们可以开始聊天了","http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh?imageView2/1/w/150/h/150/interlace/1/q/100|imageslim","","","") at chatContent.html : 440
+Could not create a template
+function: var out = ' ';
+if (it.length > 0) {
+	out += ' ';
+	for (var i = 0; i < it.length; i++) {
+		out += ' <div class="wrap"> <div class="item"> <div class="user flex-cCenter flex-def" data-id="' + (it[i].user_id) + '" tapmode onclick="openUserInfo(this)"> <div class="img"> ';
+		if (it[i].head_img_url) {
+			out += ' <img src="' + (it[i].head_img_url) + '"> ';
+		} else {
+			out += ' <img src="../../image/noimage.jpg"> ';
+		}
+		out += ' </div> <div class="text flex-def flex-cCenter"> <h3>' + (it[i].nickname) + '</h3> ';
+		if (it[i].current_company == null) {
+			out += ' ';
+			if (it[i].current_company == '') {
+				out += ' ';
+			} else if (it[i].current_position == '') {
+				out += ' <p>(' + (it[i].current_company) + ')</p> ';
+			} else if (it[i].current_company != '' && it[i].current_position != '') {
+				out += ' <p>(' + (it[i].current_company) + ' ' + (it[i].current_position) + ')</p> ';
+			} else {
+				out += ' ';
+			}
+			out += ' </div> </div> <div class="info flex-cCenter flex-def"  tapmode onclick="openInfo(this)" data-id="' + (it[i].demand_id) + '"> <div class="img"> ';
+			if (it[i].image_json[0]) {
+				out += ' <img src="' + (it[i].image_json[0]) + '"> ';
+			} else {
+				out += ' <img src="../../image/noimage.jpg"> ';
+			}
+			out += ' </div> <div class="text flex-item"> <div class="top flex-def flex-zBetween flex-cCenter"> <div class="div flex-def flex-cCenter"> ';
+			if (it[i].type == 1) {
+				out += ' <span class="label">[个人]</span> ';
+			} else {
+				out += ' <span class="label">[公司]</span> ';
+			}
+			out += ' <span class="title">' + (it[i].title) + '</span> </div> <span class="price">' + (it[i].reward) + '元</span> </div> <div class="bottom flex-def flex-zBetween"> <div class="left"> ' + (it[i].description) + ' </div> <div class="right"> ' + (it[i].create_time) + ' </div> </div> </div> </div> </div> </div> ';
+		}
+		out += ' ';
+	} else {
+		out += ' <p class="no_data">暂无数据</p> ';
+	}
+	out += ' ';
+	return out;
+	at doT.min.js: 109
