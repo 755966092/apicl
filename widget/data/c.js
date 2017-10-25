47a56616465bce2a293162b71c7accfdb0a65917
Mr.Yuan <755966092@qq.com>
2017-10-25 15:52:26 +0800

    个人中心供求圈

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,123 +1,29 @@
-{
-	"status": true,
-	"lon": 116.32842254638665,
-	"lat": 39.99800116878293,
-	"address": "北京市海淀区成府路286号",
-	"province": "北京市",
-	"city": "北京市",
-	"district": "海淀区",
-	"streetName": "成府路",
-	"streetNumber": "286号",
-	"poiList": [{
-		"name": "北京大学",
-		"uid": "ddfd7c2d8db36cf39ee3219e",
-		"address": "北京市海淀区颐和园路5号",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99774108567984,
-			"lon": 116.31617266805532
-		}
-	}, {
-		"name": "墨盒子绘本书馆",
-		"uid": "7c5e480b17bb66adadb22a4d",
-		"address": "海淀区成府路289号(近豆瓣书店)",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99786545060943,
-			"lon": 116.32830877549513
-		}
-	}, {
-		"name": "仪器耗材橡胶五金",
-		"uid": "7913cd2ef4c654eb373a713a",
-		"address": "北京市成府路276号(清华大学南门西侧)",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.9978861780755,
-			"lon": 116.32880284352561
-		}
-	}, {
-		"name": "中科科仪大厦",
-		"uid": "c778ece24776113e0c0a7020",
-		"address": "北京市海淀区成府路270号",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99780326817318,
-			"lon": 116.32902741990311
-		}
-	}, {
-		"name": "北大东门中关园学生公寓",
-		"uid": "2193d92a4cf3f2c5f7e73be0",
-		"address": "北京市海淀区北大东门中关园",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99774799484858,
-			"lon": 116.32716792749746
-		}
-	}, {
-		"name": "北京大学学生公寓",
-		"uid": "1512c5765ce62b0be812e49e",
-		"address": "北京市海淀区北京大学中关新园9号楼北100米",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.997754904016624,
-			"lon": 116.32706911389135
-		}
-	}, {
-		"name": "北京大学-技术物理系",
-		"uid": "f9fd6062ebdd8d1e0e0e6b83",
-		"address": "北京市海淀区成府路201号北京大学加速器楼",
-		"city": "北京市", 
-		"phone": "按时 ", 
-		"postcode": "",
-		"coord": {
-			"lat": 39.9986323626305,
-			"lon": 116.32699724945054
-		}
-	}, {
-		"name": "北京大学中关新园北区",
-		"uid": "64c0aee88f273ffe047ce804",
-		"address": "北京市海淀区",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99676688583297,
-			"lon": 116.32755419886674
-		}
-	}, {
-		"name": "五方院五道口店",
-		"uid": "53cebd502b7a5d782a02e16a",
-		"address": "成府路248号(近清华南路)",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.99792763298861,
-			"lon": 116.33029403067218
-		}
-	}, {
-		"name": "金泰超市(蓝旗店)",
-		"uid": "eb3675031278261b00ec0157",
-		"address": "北京市海淀区成府路113",
-		"city": "北京市",
-		"phone": "",
-		"postcode": "",
-		"coord": {
-			"lat": 39.998549453641374,
-			"lon": 116.33024013234157
-		}
-	}]
-}
-at map_frame.html: 130
+[{
+    "circle_id": 1,
+    "title": "我在第一批用户交流圈",
+    "description": "第一批我在忠实用户聚集地",
+    "image_url": "http://img.iinnet.com/system/system-head-img.png",
+    "member_count": 13,
+    "topic_count": 7
+}, {
+    "circle_id": 6,
+    "title": "设计交流",
+    "description": "设计交流",
+    "image_url": "http://img.iinnet.com/FkfF7fzoH2qxoDHw81Xy49ER8tzj",
+    "member_count": 5,
+    "topic_count": 3
+}, {
+    "circle_id": 9,
+    "title": "程序猿",
+    "description": "安卓开发程序猿或者是对开发感兴趣的同学，一起交流下啊！！！",
+    "image_url": "http://img.iinnet.com/FuaTRSlgsHU9I734GMyTExKUEWG3",
+    "member_count": 5,
+    "topic_count": 2
+}, {
+    "circle_id": 5,
+    "title": "自主招生",
+    "description": "随时了解高考自主招生最新动态及培训服务，互相交流学习，欢迎大家加入",
+    "image_url": "http://img.iinnet.com/Fn2SjcP_SAWyEh1JQE758eB0wzHC",
+    "member_count": 4,
+    "topic_count": 2
+}]
