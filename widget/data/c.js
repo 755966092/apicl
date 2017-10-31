23c8bf7a8b5c9c3f7d842971473eb5be795bb136
Mr.Yuan <755966092@qq.com>
2017-10-31 10:26:10 +0800

    发送位置10-31

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,173 +1,35 @@
-{
-    "lon": "116.32830877549513",
-    "lat": "39.99786545060943",
-    "name": "墨盒子绘本书馆",
-    "address": "海淀区成府路289号(近豆瓣书店)",
-    "imgPath": "/storage/emulated/0/UZMap/A6947696977363/download/315d6a61.png"
+数据库表格参数: {
+    "messageId": "1",
+    "receivedTime": "1509413738428",
+    "user_id": "12199",
+    "objectName": "RC:LBSMsg",
+    "messageDirection": "SEND",
+    "content_extra": "",
+    "content_text": "",
+    "content_lat": "39.997803",
+    "content_lon": "116.329027",
+    "content_imagePath": "/data/user/0/com.iinnet.apicloud/files/759d2765861e5ba5f4b8e576bfeaee33/location/1",
+    "head_img_url": "/storage/emulated/0/Android/data/com.iinnet.apicloud/cache/disk/thumb/2c69de7b",
+    "filePath": "undefined",
+    "duration": "",
+    "thumbPath": "undefined"
 }
-at chatContent.html: 384
+at chatContent.html: 586
 
-接受消息后: {
-    "value": {
-        "targetId": "47d2b511386521e8235274b1287e5525",
-        "msgContent": {
-            "result": {
-                "left": 0,
-                "message": {
-                    "content": {
-                        "extra": "",
-                        "latitude": 39.99786376953125,
-                        "longitude": 116.32830810546875,
-                        "poi": "海淀区成府路289号(近豆瓣书店)"
-                    },
-                    "conversationType": "PRIVATE",
-                    "extra": "",
-                    "messageDirection": "RECEIVE",
-                    "messageId": 11,
-                    "objectName": "RC:LBSMsg",
-                    "receivedTime": 1509073114619,
-                    "senderUserId": "47d2b511386521e8235274b1287e5525",
-                    "sentStatus": "SENT",
-                    "sentTime": 1509073111119,
-                    "targetId": "47d2b511386521e8235274b1287e5525"
-                }
-            }
-        }
-    }
+数据库表格参数: {
+    "messageId": "1",
+    "receivedTime": "1509413738428",
+    "user_id": "12199",
+    "objectName": "RC:LBSMsg",
+    "messageDirection": "SEND",
+    "content_extra": "",
+    "content_text": "",
+    "content_lat": "39.997803",
+    "content_lon": "116.329027",
+    "content_imagePath": "/data/user/0/com.iinnet.apicloud/files/759d2765861e5ba5f4b8e576bfeaee33/location/1",
+    "head_img_url": "/storage/emulated/0/Android/data/com.iinnet.apicloud/cache/disk/thumb/2c69de7b",
+    "filePath": "undefined",
+    "duration": "",
+    "thumbPath": "undefined"
 }
-at chatContent.html: 425
-
-新建标签参数: {
-    "status": "prepare",
-    "result": {
-        "message": {
-            "extra": "",
-            "sentStatus": "SENDING",
-            "messageId": 17,
-            "sentTime": 1509073436290,
-            "conversationType": "PRIVATE",
-            "messageDirection": "SEND",
-            "receivedStatus": "UNREAD",
-            "content": {
-                "extra": "",
-                "latitude": 39.99711227416992,
-                "longitude": 116.32665252685547,
-                "poi": "北京市海淀区",
-                "imagePath": "/var/mobile/Containers/Data/Application/556D8ADB-E1B3-4AE6-87DE-39C46F90CB3A/Documents/da39a3ee5e6b4b0d3255bfef95601890afd80709"
-            },
-            "objectName": "RC:LBSMsg",
-            "senderUserId": "47d2b511386521e8235274b1287e5525",
-            "receivedTime": 0,
-            "targetId": "759d2765861e5ba5f4b8e576bfeaee33"
-        }
-    }
-}
-
-
-{
-    "conversationType": "PRIVATE",
-    "targetId": "759d2765861e5ba5f4b8e576bfeaee33",
-    "latitude": 39.997802734375,
-    "longitude": 116.32902526855469,
-    "poi": "北京市海淀区成府路270号",
-    "imagePath": "fs:///var/mobile/Containers/Data/Application/556D8ADB-E1B3-4AE6-87DE-39C46F90CB3A/Library/Caches/APICloud/Cache/35d6b0afa04215d4ac3f68feb070c683",
-    "extra": "fs:///var/mobile/Containers/Data/Application/556D8ADB-E1B3-4AE6-87DE-39C46F90CB3A/Library/Caches/APICloud/Cache/35d6b0afa04215d4ac3f68feb070c683"
-} {
-    "conversationType": "PRIVATE",
-    "targetId": "759d2765861e5ba5f4b8e576bfeaee33",
-    "latitude": 39.997886657714844,
-    "longitude": 116.32880401611328,
-    "poi": "北京市成府路276号(清华大学南门西侧)",
-    "imagePath": "http://api.map.baidu.com/staticimage/v2?ak=uVI0viOM0c4W64xi19cVxza3q9IniAIy&width=443&height=172&center=116.32880401611328,39.997886657714844&markers=116.32880401611328,39.997886657714844&zoom=17&markerStyles=m,,0xff0000",
-    "extra": "http://api.map.baidu.com/staticimage/v2?ak=uVI0viOM0c4W64xi19cVxza3q9IniAIy&width=443&height=172&center=116.32880401611328,39.997886657714844&markers=116.32880401611328,39.997886657714844&zoom=17&markerStyles=m,,0xff0000"
-}
-新建标签参数: {
-    "status": "prepare",
-    "result": {
-        "message": {
-            "extra": "",
-            "sentStatus": "SENDING",
-            "messageId": 24,
-            "sentTime": 1509075346823,
-            "conversationType": "PRIVATE",
-            "messageDirection": "SEND",
-            "receivedStatus": "UNREAD",
-            "content": {
-                "extra": "http://api.map.baidu.com/staticimage/v2?ak=uVI0viOM0c4W64xi19cVxza3q9IniAIy&width=443&height=172&center=116.32880401611328,39.997886657714844&markers=116.32880401611328,39.997886657714844&zoom=17&markerStyles=m,,0xff0000",
-                "latitude": 39.997886657714844,
-                "longitude": 116.32880401611328,
-                "poi": "北京市成府路276号(清华大学南门西侧)",
-                "imagePath": "/var/mobile/Containers/Data/Application/556D8ADB-E1B3-4AE6-87DE-39C46F90CB3A/Documents/da39a3ee5e6b4b0d3255bfef95601890afd80709"
-            },
-            "objectName": "RC:LBSMsg",
-            "senderUserId": "47d2b511386521e8235274b1287e5525",
-            "receivedTime": 0,
-            "targetId": "759d2765861e5ba5f4b8e576bfeaee33"
-        }
-    }
-}
-
-{
-    "conversationType": "PRIVATE",
-    "targetId": "47d2b511386521e8235274b1287e5525",
-    "latitude": 39.99709162067177,
-    "longitude": 116.30137757630598,
-    "poi": "新建宫门路北50米",
-    "imagePath": "http://api.map.baidu.com/staticimage/v2?ak=uVI0viOM0c4W64xi19cVxza3q9IniAIy&width=443&height=172&center=116.30137757630598,39.99709162067177&markers=116.30137757630598,39.99709162067177&zoom=17&markerStyles=m,,0xff0000",
-    "extra": ""
-}
-at chatContent.html: 497
-接受标签参数: {
-    "targetId": "47d2b511386521e8235274b1287e5525",
-    "msgContent": {
-        "result": {
-            "left": 0,
-            "message": {
-                "content": {
-                    "extra": "",
-                    "imagePath": "/data/user/0/com.iinnet.apicloud/files/759d2765861e5ba5f4b8e576bfeaee33/location/36",
-                    "latitude": 40.01628875732422,
-                    "longitude": 116.3145980834961,
-                    "poi": "北京市海淀区清华西路28号"
-                },
-                "conversationType": "PRIVATE",
-                "extra": "",
-                "messageDirection": "RECEIVE",
-                "messageId": 36,
-                "objectName": "RC:LBSMsg",
-                "receivedTime": 1509083423474,
-                "senderUserId": "47d2b511386521e8235274b1287e5525",
-                "sentStatus": "SENT",
-                "sentTime": 1509083419794,
-                "targetId": "47d2b511386521e8235274b1287e5525"
-            }
-        }
-    }
-}
-at chatContent.html: 529
-
-新建标签参数: {
-    "result": {
-        "message": {
-            "content": {
-                "extra": "",
-                "imagePath": "/data/user/0/com.iinnet.apicloud/files/759d2765861e5ba5f4b8e576bfeaee33/location/49",
-                "latitude": 39.99786545060943,
-                "longitude": 116.32830877549513,
-                "poi": "海淀区成府路289号(近豆瓣书店)"
-            },
-            "conversationType": "PRIVATE",
-            "extra": "",
-            "messageDirection": "SEND",
-            "messageId": 49,
-            "objectName": "RC:LBSMsg",
-            "receivedTime": 0,
-            "senderUserId": "759d2765861e5ba5f4b8e576bfeaee33",
-            "sentStatus": "SENDING",
-            "sentTime": 1509085659230,
-            "targetId": "47d2b511386521e8235274b1287e5525"
-        }
-    },
-    "status": "prepare"
-}
-at chatContent.html: 516
+at chatContent.html: 586
