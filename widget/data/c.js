2286b79a14c3a42b2765998ab58312df1c34e34a
Mr.Yuan <755966092@qq.com>
2017-11-07 18:05:17 +0800

    编辑公司页面11-07

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,8 +1,45 @@
 {
-    "token": "69e29686-52c6-51f1-81bc-a94c1ff1d1d9",
-    "company_id": 19,
-    "identity": "",
-    "authorization": "",‘“
-    "business_license": "http://img.iinnet.com/FtEh3SxROrTAVrVCRL7t6SyOgAK1"
+    "code": 200,
+    "msg": "",
+    "time": "1510040083",
+    "data": {
+        "list": [{
+            "work_id": 31,
+            "user_id": 18989,
+            "company_id": 20,
+            "company_name": "跨考",
+            "position": "前端",
+            "description": "测试",
+            "start_date": "2017-10-01",
+            "end_date": "0000-00-00",
+            "is_default": -1,
+            "is_verify": -1,
+            "admin_id": null
+        }, {
+            "work_id": 30,
+            "user_id": 18989,
+            "company_id": 19,
+            "company_name": "第一学",
+            "position": "前端",
+            "description": "测试开发",
+            "start_date": "2017-09-04",
+            "end_date": "0000-00-00",
+            "is_default": -1,
+            "is_verify": 0,
+            "admin_id": 18989
+        }, {
+            "work_id": 25,
+            "user_id": 18989,
+            "company_id": 0,
+            "company_name": "第一学教育科技有限公司",
+            "position": "前端工程师",
+            "description": "开发我在",
+            "start_date": "2017-04-15",
+            "end_date": "0000-00-00",
+            "is_default": 1,
+            "is_verify": null,
+            "admin_id": null
+        }]
+    }
 }
-at me_gs_wrz.html: 161
+at me_gs.html: 165
