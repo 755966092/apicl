2acbc3873b045b388e2aa8cd0639c09cbc9f90d4
Mr.Yuan <755966092@qq.com>
2017-11-08 14:31:44 +0800

    公司信息页面11-08

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,39 +1,14 @@
 {
-    "A": [{
-        "industry_id": 8,
-        "name": "啊建筑",
-        "letter": "AJZ",
-        "flag": "A",
-        "initial": "A",
-        "title": "啊建筑",
-        "titleSize": 15
-    }],
-    "B": [{
-        "industry_id": 8,
-        "name": "吧建筑",
-        "letter": "BJZ",
-        "flag": "B",
-        "initial": "B",
-        "title": "吧建筑",
-        "titleSize": 15
-    }],
-    "C": [{
-        "industry_id": 8,
-        "name": "吃建筑",
-        "letter": "CJZ",
-        "flag": "C",
-        "initial": "C",
-        "title": "吃建筑",
-        "titleSize": 15
-    }],
-    "J": [{
-        "industry_id": 8,
-        "name": "建筑",
-        "letter": "JZ",
-        "flag": "J",
-        "initial": "J",
-        "title": "建筑",
-        "titleSize": 15
-    }]
+    "code": 200,
+    "msg": "",
+    "time": "1510119624",
+    "data": {
+        "list": [{
+            "start_date": "2017-09-04",
+            "nickname": "袁壮2",
+            "position": "前端",
+            "name": "简称"
+        }]
+    }
 }
-at me_gs_industry.html: 105
+at me_gs_management.html: 48
