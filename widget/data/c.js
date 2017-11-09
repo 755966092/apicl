008c3fe4a260133ad8ce76e7d96779da56d80553
Mr.Yuan <755966092@qq.com>
2017-11-09 17:27:42 +0800

    人脉关系图备注头像替换用户自己头像

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,19 +1,54 @@
 {
-    "extra": "",
-    "sentStatus": "SENDING",
-    "messageId": 8,
-    "sentTime": 1510206402375,
-    "conversationType": "PRIVATE",
-    "messageDirection": "SEND",
-    "receivedStatus": "UNREAD",
-    "content": {
-        "thumbPath": "/var/mobile/Containers/Data/Application/49845A9B-2D4B-4BD3-8A13-E0873BDCE00C/Documents/14ea164c8bd98590690fbf8ca91a51e40ae75a52",
-        "extra": "",
-        "imageUrl": ""
-    },
-    "objectName": "RC:ImgMsg",
-    "senderUserId": "759d2765861e5ba5f4b8e576bfeaee33",
-    "receivedTime": 0,
-    "targetId": "47d2b511386521e8235274b1287e5525"
+    "code": 200,
+    "msg": "",
+    "time": "1510219355",
+    "data": {
+        "self_user_id": 9969,
+        "user_info": {
+            "204": {
+                "user_id": 204,
+                "nickname": "佛易道",
+                "head_img_url": "http://img.iinnet.com/Fq_SwUAj6_nn-cl3NOFcOSwfQApE",
+                "remark_nickname": "公司—赵老师",
+                "remark_head_img_url": ""
+            },
+            "236": {
+                "user_id": 236,
+                "nickname": "Una",
+                "head_img_url": "http://img.iinnet.com/FtiKpkN07pcKR-f77pown3PgTVy9",
+                "remark_nickname": "杨慧",
+                "remark_head_img_url": ""
+            },
+            "1034": {
+                "user_id": 1034,
+                "nickname": "Aimee",
+                "head_img_url": "http://img.iinnet.com/FuMK5Vw5w8gp2pPuGyPDIm1lcW_Z",
+                "remark_nickname": "杨玉招",
+                "remark_head_img_url": "http://img.iinnet.com/Fp2hA07ym-t17PztncYHrR4HZiXJ"
+            },
+            "1885": {
+                "user_id": 1885,
+                "nickname": "佛易道科",
+                "head_img_url": "http://img.iinnet.com/FmDMt6z5XeEC4OPkS7bFX9_ZnUdX",
+                "remark_nickname": "公司—赵老师",
+                "remark_head_img_url": ""
+            },
+            "9969": {
+                "user_id": 9969,
+                "nickname": "大中小18765",
+                "head_img_url": "http://img.iinnet.com/FnUe1oBtIUzuEJJAEypX1vsUNHkh",
+                "remark_nickname": null,
+                "remark_head_img_url": null
+            }
+        },
+        "relationship_info": [
+            ["9969", "204"],
+            ["204", "1034"],
+            ["9969", "236"],
+            ["236", "1034"],
+            ["9969", "1885"],
+            ["1885", "1034"]
+        ]
+    }
 }
-at 1631
+at txl_friendInfo_networkOfPeople_frame.html: 106
