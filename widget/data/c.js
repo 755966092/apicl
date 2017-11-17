f1185992d74d3f4400a5112e697be962a4fe45c7
Mr.Yuan <755966092@qq.com>
2017-11-17 18:19:10 +0800

    11-17

--- widget/data/c.js
+++ widget/data/c.js
@@ -1,45 +1,15 @@
 {
     "code": 200,
     "msg": "",
-    "time": "1510710531",
+    "time": "1510723511",
     "data": {
         "list": [{
-            "work_id": 32,
-            "user_id": 18536,
-            "company_id": 21,
-            "company_name": "测试公司",
-            "position": "开发",
-            "description": "测试",
-            "start_date": "2017-10-15",
-            "end_date": "0000-00-00",
-            "is_default": -1,
-            "is_verify": -1,
-            "admin_id": null
-        }, {
-            "work_id": 27,
-            "user_id": 18536,
-            "company_id": 0,
-            "company_name": "清华",
-            "position": "清华",
-            "description": "测试",
-            "start_date": "2017-07-01",
-            "end_date": "2017-07-29",
-            "is_default": -1,
-            "is_verify": null,
-            "admin_id": null
-        }, {
-            "work_id": 23,
-            "user_id": 18536,
-            "company_id": 0,
-            "company_name": "第一学",
-            "position": "开发",
-            "description": "开发，我在",
-            "start_date": "2017-04-12",
-            "end_date": "0000-00-00",
-            "is_default": 1,
-            "is_verify": null,
-            "admin_id": null
+            "user_id": 18989,
+            "start_date": "2017-10-01",
+            "nickname": "袁壮2",
+            "position": "前端",
+            "name": "跨考"
         }]
     }
 }
-at supply_releaseCompany_frame.html: 88
+at me_gs_companyPage_frame3.html: 139
