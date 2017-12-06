927288d3d26efd6f4660af35498c426eef9afdf8
Mr.Yuan <755966092@qq.com>
2017-12-06 11:16:25 +0800

    供求订单筛选按钮12-06

--- widget/script/common.js
+++ widget/script/common.js
@@ -3,19 +3,19 @@
  */
 
 //rem转换
-// (function(doc, win) {
-// 	var docEl = doc.documentElement,
-// 		resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
-// 		recalc = function() {
-// 			var clientWidth = docEl.clientWidth;
-// 			if (!clientWidth) return;
-// 			docEl.style.fontSize = 100 * (clientWidth / 750) + "px";
-// 		};
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+			docEl.style.fontSize = 100 * (clientWidth / 750) + "px";
+		};
 
-// 	if (!doc.addEventListener) return;
-// 	win.addEventListener(resizeEvt, recalc, false);
-// 	doc.addEventListener("DOMContentLoaded", recalc, false);
-// })(document, window);
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener("DOMContentLoaded", recalc, false);
+})(document, window);
 
 
 
